c0fc53edd4a6c0ad8b7bf68a8fff8634fcd54ccf
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-06 03:08:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,64 @@
-from pyrogram import Client, filters
+from telegram import Update
+from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackContext
 from pymongo import MongoClient
-import random
+import requests
+import os
 
-TOKEN = '6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs'
+# Initialize MongoDB client
 client = MongoClient('mongodb+srv://shekharhatture:kUi2wj2wKxyUbbG1@cluster0.od4v7eo.mongodb.net/?retryWrites=true&w=majority')
-db = client.get_database('test')
-collection = db.get_collection('images')
+db = client['mydatabase']
+collection = db['mycollection']
 
-app = Client("my_bot", bot_token=TOKEN, api_id=24427150, api_hash="9fcc60263a946ef550d11406667404fa")
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hi! Send me a photo with /upload [image name]')
 
+def upload(update: Update, context: CallbackContext) -> None:
+    # Check if photo is included in the message
+    if update.message.photo:
+        # Get file_id of the photo
+        file_id = update.message.photo[-1].file_id
 
-@app.on_message(filters.command("upload"))
-def upload(client, message):
-    if message.reply_to_message and message.reply_to_message.photo:
-        file_id = message.reply_to_message.photo.file_id
-        image_name = message.text.split()[1]
-        collection.insert_one({'file_id': file_id, 'name': image_name})
+        # Get image name from the command
+        image_name = ' '.join(context.args)
 
-@app.on_message(filters.group)
-def send_image(client, message):
-    count = collection.count_documents({})
-    if count % 10 == 0:
-        doc = random.choice(list(collection.find()))
-        app.send_photo(message.chat.id, doc['file_id'], caption="Guess the name and get coins")
+        # Get the photo file
+        photo_file = context.bot.get_file(file_id)
 
-@app.on_message(filters.command("guess"))
-def guess(client, message):
-    guess_name = message.text.split()[1]
-    doc = collection.find_one({'name': guess_name})
-    if doc:
-        app.send_message(message.chat.id, f"{message.from_user.first_name} guessed the image correctly and got 5 coins")
-        collection.delete_one({'_id': doc['_id']})
+        # Download the photo to local directory
+        photo_file.download(image_name)
 
-app.run()
+        # Save the photo in MongoDB
+        with open(image_name, "rb") as f:
+            binary_data = f.read()
+
+        # Insert into MongoDB (you might want to use GridFS if the file is large)
+        collection.insert_one({"image_name": image_name, "image_data": binary_data})
+
+        # Delete the local file as it's already saved in MongoDB
+        os.remove(image_name)
+
+        update.message.reply_text('Upload done')
+    else:
+        update.message.reply_text('Please reply a photo with /upload [image name]')
+
+def main() -> None:
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Add command handlers
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("upload", upload))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
