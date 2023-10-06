cfc62b99069b6df3f9139c0ed04a41d931d8b9f8
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-06 14:21:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,107 @@
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from PIL import Image, ImageDraw, ImageFont
+import requests
+from io import BytesIO
+import random
+from pymongo import MongoClient
+
+# Connect to your MongoDB database
+client = MongoClient('mongodb+srv://shekharhatture:kUi2wj2wKxyUbbG1@cluster0.od4v7eo.mongodb.net/?retryWrites=true&w=majority')
+db = client['logooo']
+collection = db['logodb']
+
+# Define fonts list
+fonts = ['Assets/adrip1.ttf']
+
+# Define authorized user ids
+authorized_ids = [6404226395, 5654523936]
+
+def add(update: Update, context: CallbackContext) -> None:
+    # Check if the command is used by an authorized user
+    if update.message.from_user.id not in authorized_ids:
+        update.message.reply_text('Sojaaaa...')
+        return
+
+    # Check if the message has a photo
+    if not update.message.photo:
+        update.message.reply_text('Photo Bhej Aur Category Likh.')
+        return
+
+    # Check if a category is provided
+    if not context.args:
+        update.message.reply_text('Category Kya tera baap Likhega?')
+        return
+
+    category = context.args[0]
+
+    # Download the photo
+    photo_path = update.message.photo[-1].get_file().download()
+
+    # Insert the photo into the database with the specified category
+    with open(photo_path, 'rb') as photo_file:
+        try:
+            collection.insert_one({'category': category, 'photo': photo_file.read()})
+            update.message.reply_text('Photo added successfully.')
+        except Exception as e:
+            update.message.reply_text(f'Failed to add photo: {e}')
+
+def logo(update: Update, context: CallbackContext) -> None:
+    user_input_text = " ".join(context.args)
+
+    if not user_input_text:
+        update.message.reply_text('Please provide some text...')
+        return
+
+    keyboard = [
+        [InlineKeyboardButton("Boy", callback_data='boy'),
+         InlineKeyboardButton("Girl", callback_data='girl'),
+         InlineKeyboardButton("Scenary", callback_data='scenary')]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+
+    # Fetch random image from the database based on the selected category
+    image_data = collection.find_one({'category': query.data})
+
+    query.edit_message_text(text="Wait for some seconds...")
+
+    # Open image and draw text
+    img = Image.open(BytesIO(image_data['photo']))
+    d = ImageDraw.Draw(img)
+    
+    font = ImageFont.truetype(random.choice(fonts), 150)
+    
+    # Calculate width and height of the text
+    w, h = d.textsize(user_input_text, font=font)
+
+    # Calculate x and y coordinates to center the text
+    x = (img.width - w) / 2
+    y = (img.height - h) / 2
+
+    d.text((x,y), user_input_text, fill=(255,255,255), font=font)
+
+    # Save and send the image
+    img.save('output.png')
+    
+    # Send the final image
+    with open('output.png', 'rb') as photo:
+        query.message.reply_photo(photo=photo)
+
+def main() -> None:
+    updater = Updater("6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs", use_context=True)
+
+    updater.dispatcher.add_handler(CommandHandler('add', add))
+    updater.dispatcher.add_handler(CommandHandler('logo', logo))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
+    updater.start_polling()
+
+if __name__ == '__main__':
+    main()
