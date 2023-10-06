1e4f2a2d267a2c6ac6f22ff44290a1f73b78ebab
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-06 09:54:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,49 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-import schedule
-import time
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
+import random
 
-class ImageBot:
-    def __init__(self):
-        self.updater = Updater(token='6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs', use_context=True)
-        self.dispatcher = self.updater.dispatcher
-        self.images = [('https://graph.org/file/bce79a4a7b2e5e73dc37a.jpg', 'Naruto Uzumaki'), ('https://graph.org/file/314324a8e1831137c8f94.jpg', 'Hinata')]  # Add your image URLs here
-        self.current_image = None
+# Replace 'TOKEN' with your Bot's API token.
+updater = Updater(token='6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs', use_context=True)
 
-    def start(self, update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+# This is your list of quiz questions, answers and images.
+quiz_list = [
+    {"question": "Question 1", "options": ["Option 1", "Option 2", "Option 3"], "answer": 0, "image": "https://graph.org/file/bce79a4a7b2e5e73dc37a.jpg"},
+    {"question": "Question 2", "options": ["Option 1", "Option 2", "Option 3"], "answer": 1, "image": "https://graph.org/file/bce79a4a7b2e5e73dc37a.jpg"},
+    # Add more questions here.
+]
 
-    
-    def send_image(self):
-       if self.images:  # Check if there are any images left
-           self.current_image = self.images.pop(0)  # Get the next image from the list
-           self.updater.bot.send_photo(chat_id=chat_id, photo=self.current_image[0])
+# This will store the coins for each user.
+user_data = {}
 
+def start(update: Update, context: CallbackContext) -> None:
+    quiz = random.choice(quiz_list)
+    context.chat_data['quiz'] = quiz
 
+    keyboard = [[InlineKeyboardButton(option, callback_data=str(i))] for i, option in enumerate(quiz["options"])]
 
+    with open(quiz["image"], 'rb') as f:
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=f)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=quiz["question"], reply_markup=InlineKeyboardMarkup(keyboard))
 
-    def run(self):
-        start_handler = CommandHandler('start', self.start)
-        self.dispatcher.add_handler(start_handler)
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
 
-        schedule.every(1).minutes.do(self.send_image)
+    if int(query.data) == context.chat_data['quiz']["answer"]:
+        user_id = update.effective_user.id
+        if user_id not in user_data:
+            user_data[user_id] = 0
+        user_data[user_id] += 1
+        query.edit_message_text(text=f"Correct! You now have {user_data[user_id]} coins.")
+    else:
+        query.edit_message_text(text="Sorry, that's incorrect.")
 
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+def main() -> None:
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
-    bot = ImageBot()
-    bot.run()
+    main()
