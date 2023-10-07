7bd7e7dbcb6ce7e91bce7e194284513b9a77790a
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-07 08:55:04 +0530

    Add files via upload

---
+++ Robot/__init__ (1).py
@@ -0,0 +1,27 @@
+import logging
+import os
+import sys
+import time
+
+import telegram.ext as tg
+
+StartTime = time.time()
+
+# enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+
+WORKERS = 8
+
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+dispatcher = updater.dispatcher
+
+tg.RegexHandler = CustomRegexHandler
+tg.CommandHandler = CustomCommandHandler
+tg.MessageHandler = CustomMessageHandler
