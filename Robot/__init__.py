b75c7b4fff9f8fb85074963a476c403c6334d4c0
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-07 09:07:20 +0530

    Update __init__.py

--- Robot/__init__.py
+++ Robot/__init__.py
@@ -18,7 +18,7 @@ logging.getLogger("apscheduler").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 WORKERS = 8
-
+TOKEN = "6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs"
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 dispatcher = updater.dispatcher
 
