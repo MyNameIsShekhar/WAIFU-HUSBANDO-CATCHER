3b8535fb69ef36b111ab77e9048a02d1c26ecdc8
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-07 09:12:07 +0530

    Update __init__.py

--- Robot/__init__.py
+++ Robot/__init__.py
@@ -22,6 +22,3 @@ TOKEN = "6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs"
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 dispatcher = updater.dispatcher
 
-tg.RegexHandler = CustomRegexHandler
-tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
