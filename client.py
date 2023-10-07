ab33acd1dd6f0c698e70fbe055960c971769ba47
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-07 08:14:44 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,10 @@
+import logging
+import os
+import sys
+import time
+import telegram.ext as tg
+
+TOKEN = "6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs"
+
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+dispatcher = updater.dispatcher
