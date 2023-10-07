7a57284ae95ee2e18008befa66baaeaf4f713bf5
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-08 03:09:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,67 +1,45 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters
-import random
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
 
-# Dictionary to store character information
-characters = {
-    "character1": {
-        "image": "https://graph.org/file/60dcb0e5dc76ad24d52b8.jpg", 
-        "options": ["Obito", "Madara", "Madara", "Lmao", "Lmaoo"],
-        "correct": "Obito"
+# Define your images, options, and correct answers here
+quiz = [
+    {
+        "image_url": "https://graph.org/file/cb384e79bc3ff2a36e1fa.jpg",
+        "options": ["Salad", "Beef Steak", "Hamburger"],
+        "answer": "Hamburger"
     },
-    "character2": {
-        "image": "https://graph.org/file/b942cc9107e5909de5498.jpg", 
-        "options": ["Mitsuri", "Rendi", "Rednii", "lkxaa4", "option5"],
-        "correct": "Mitsuri"
+    {
+        "image_url": "https://graph.org/file/60dcb0e5dc76ad24d52b8.jpg",
+        "options": ["Pizza", "Pasta", "Lasagna"],
+        "answer": "Pizza"
     },
-    # Add more characters as needed
-}
+    # Add more images and options here
+]
 
-# Dictionary to store user attempts
-user_attempts = {}
 
-# Counter for group messages
-message_counter = 0
-
-def send_character(update: Update, context: CallbackContext) -> None:
-    global message_counter
-    message_counter += 1
-
-    if message_counter % 20 == 0:
-        character = random.choice(list(characters.keys()))
-        image_url = characters[character]["image"]
-        options = characters[character]["options"]
-        
-        keyboard = [[InlineKeyboardButton(option, callback_data=character)] for option in options]
-        
+def start(update: Update, context: CallbackContext) -> None:
+    for i, q in enumerate(quiz):
+        question = f"What is this food? (Question {i+1})"
+        keyboard = [[InlineKeyboardButton(o, callback_data=f"{i}:{o}")] for o in q["options"]]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        
-        context.bot.send_photo(chat_id=update.effective_chat.id, photo=image_url, caption="Choose correct name of the character", reply_markup=reply_markup)
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=q["image_url"], caption=question, reply_markup=reply_markup)
 
 def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     query.answer()
-    
-    character = query.data
-    user_id = query.from_user.id
-    
-    if user_id in user_attempts and user_attempts[user_id] == character:
-        query.edit_message_text(text="You've already tried this one!")
-        return
-    
-    user_attempts[user_id] = character
-    
-    if query.message.caption == characters[character]["correct"]:
-        # User was correct. Add 5 coins to their total in the database here.
-        query.edit_message_text(text=f"Correct! You get 5 coins. Your new total is {new_total}.")
+    i, answer = query.data.split(":")
+    if answer == quiz[int(i)]["answer"]:
+        query.edit_message_text(text=f"Correct! You've earned a coin.")
     else:
-        query.edit_message_text(text="Sorry, that's not correct.")
+        query.edit_message_text(text=f"Sorry, that's not correct.")
 
 def main() -> None:
-    updater = Updater("6656458442:AAGJ1nKC2qil9SMU3NbElluHSmHJrN8oZsg", use_context=True)
+    updater = Updater("6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs", use_context=True)
 
-    updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, send_character))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CallbackQueryHandler(button))
 
     updater.start_polling()
 
