47df45b76ef720dbf99b24ea8e731ad9fd3e3778
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-08 13:43:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,69 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
-import logging
-import requests 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.error import BadRequest
+import random
 
-logger = logging.getLogger(__name__)
+# List of dictionaries with image links and their names
+characters = [
+    {"name": "Naruto", "image_url": "https://graph.org/file/9d78c4029a5d0aea6e7d0.jpg", "options": ["Rendi", "amedni", "bkl", "bsdk"]},
+    {"name": "Hinata", "image_url": "https://graph.org/file/314324a8e1831137c8f94.jpg", "options": ["gandu", "choda", "moda", "lofa"]},
+    # Add more characters as needed
+]
 
-def start(update: Update, context: CallbackContext) -> None:
-    keyboard = [
-        [InlineKeyboardButton("Option 1", callback_data='1'),
-         InlineKeyboardButton("Option 2", callback_data='2')]
-    ]
+# Dictionary to keep track of user attempts
+user_attempts = {}
 
+def random(update: Update, context: CallbackContext) -> None:
+    # Select a random character
+    correct_character = random.choice(characters)
+    
+    # Create a list of options including the correct one
+    options = correct_character["options"].copy()
+    options.append(correct_character["name"])
+    
+    # Shuffle the options to randomize the correct answer's position
+    random.shuffle(options)
+    
+    # Create an inline keyboard with the character names as buttons
+    keyboard = [[InlineKeyboardButton(option, callback_data=option) for option in options]]
+    
     reply_markup = InlineKeyboardMarkup(keyboard)
-
-    # Send an image from a URL
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo='https://graph.org/file/60dcb0e5dc76ad24d52b8.jpg', reply_markup=reply_markup)
+    
+    # Send the question message with the inline keyboard
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=correct_character["image_url"], caption="Choose Correct Name Of The Character", reply_markup=reply_markup)
 
 def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
-
-    # Answer the query
-    query.answer("You're not eligible." , show_alert=True)
+    
+    # Check if the user has already attempted to answer
+    if query.from_user.id in user_attempts and user_attempts[query.from_user.id]:
+        query.answer("You've already tried", show_alert=True)
+        return
+    
+    # Check if the selected option is correct
+    for character in characters:
+        if character["name"] == query.data:
+            try:
+                query.edit_message_text(text=f"Correct! The character is {query.data}. Well done {query.from_user.first_name}!")
+                user_attempts[query.from_user.id] = True
+            except BadRequest:
+                pass
+            return
+    
+    # If the selected option is incorrect
+    query.answer("You're wrong", show_alert=True)
+    user_attempts[query.from_user.id] = True
 
 def main() -> None:
-    updater = Updater("6684851741:AAFdjonPh64ISqm0nBjS_xSI1fqCww6oBiw", use_context=True)
+    updater = Updater("6504156888:AAEg_xcxqSyYIbyCZnH6zJmwMNZm3DFTmJs", use_context=True)
 
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start))
-
+    dispatcher.add_handler(CommandHandler('random', random))
+    
     dispatcher.add_handler(CallbackQueryHandler(button))
 
     updater.start_polling()
-
-    updater.idle()
 
 if __name__ == '__main__':
     main()
