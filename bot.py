34121d44508a6ee8b1db9065ee064f94b9a12b19
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-08 14:43:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, Updater 
 from telegram.error import BadRequest
 import random
+
 from pymongo import MongoClient
 
 # MongoDB setup
