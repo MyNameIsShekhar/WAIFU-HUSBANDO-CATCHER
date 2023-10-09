c6eecde6e2448ece75a1d62d912feea12c529ecd
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-09 18:01:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,6 +27,111 @@ sent_characters = {}
 # Keep track of the user who guessed correctly first in each group
 first_correct_guesses = {}
 
+
+def upload(update: Update, context: CallbackContext) -> None:
+    # Check if user is a sudo user
+    if str(update.effective_user.id) not in sudo_users:
+        update.message.reply_text('You do not have permission to use this command.')
+        return
+
+    try:
+        # Extract arguments
+        args = context.args
+        if len(args) != 3:
+            update.message.reply_text('Incorrect format. Please use: /upload img_url Character-Name Anime-Name')
+            return
+
+        # Replace '-' with ' ' in character name
+        character_name = args[1].replace('-', ' ')
+
+        # Check if image URL is valid
+        try:
+            urllib.request.urlopen(args[0])
+        except:
+            update.message.reply_text('Invalid image URL.')
+            return
+
+        # Generate ID
+        id = str(collection.count_documents({}) + 1).zfill(4)
+
+        # Insert new character
+        character = {
+            'img_url': args[0],
+            'name': character_name,
+            'anime': args[2],
+            'id': id
+        }
+        collection.insert_one(character)
+        
+        update.message.reply_text('Successfully uploaded.')
+
+        # Send message to channel
+        context.bot.send_photo(
+            chat_id='-1001865838715',
+            photo=args[0],
+            caption=f'<b>Character Name:</b> {character_name}\n<b>Anime Name:</b> {args[2]}\n<b>ID:</b> {id}\nAdded by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+            parse_mode='HTML'
+        )
+    except Exception as e:
+        update.message.reply_text('Unsuccessfully uploaded.')
+
+
+def anime(update: Update, context: CallbackContext) -> None:
+    try:
+        # Get all unique anime names
+        anime_names = collection.distinct('anime')
+
+        # Send message with anime names
+        update.message.reply_text('\n'.join(anime_names))
+    except Exception as e:
+        update.message.reply_text('Failed to fetch anime names.')
+
+def delete(update: Update, context: CallbackContext) -> None:
+    # Check if user is a sudo user
+    if str(update.effective_user.id) not in sudo_users:
+        update.message.reply_text('You do not have permission to use this command.')
+        return
+
+    try:
+        # Extract arguments
+        args = context.args
+        if len(args) != 1:
+            update.message.reply_text('Incorrect format. Please use: /delete ID')
+            return
+
+        # Delete character with given ID
+        result = collection.delete_one({'id': args[0]})
+
+        if result.deleted_count > 0:
+            update.message.reply_text('Successfully deleted.')
+        else:
+            update.message.reply_text('No character found with given ID.')
+    except Exception as e:
+        update.message.reply_text('Failed to delete character.')
+
+def total(update: Update, context: CallbackContext) -> None:
+    try:
+        # Extract arguments
+        args = context.args
+        if len(args) != 1:
+            update.message.reply_text('Incorrect format. Please use: /total Anime-Name')
+            return
+
+        # Replace '-' with ' ' in anime name
+        anime_name = args[0].replace('-', ' ')
+
+        # Get all characters of the given anime
+        characters = collection.find({'anime': anime_name})
+
+        # Create a list of character names and IDs
+        character_list = [f'Character Name: {character["name"]}\nID: {character["id"]}' for character in characters]
+
+        # Send message with character names and IDs
+        update.message.reply_text('\n\n'.join(character_list))
+    except Exception as e:
+        update.message.reply_text('Failed to fetch characters.')
+
+
 def message_counter(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
 
@@ -130,6 +235,10 @@ def main() -> None:
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, message_counter))
     dispatcher.add_handler(CommandHandler('guess', guess))
     dispatcher.add_handler(CommandHandler('leaderboard', leaderboard))
+    dispatcher.add_handler(CommandHandler('upload', upload))
+    dispatcher.add_handler(CommandHandler('anime', anime))
+    dispatcher.add_handler(CommandHandler('delete', delete))
+    dispatcher.add_handler(CommandHandler('total', total))
 
     updater.start_polling()
 
