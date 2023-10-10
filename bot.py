bef9f02f72e3d4533a783ae704dcb7bdbf08032d
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-10 08:29:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -225,7 +225,7 @@ def guess(update: Update, context: CallbackContext) -> None:
             update.message.reply_text('Your guess is incorrect.')
 
 def main() -> None:
-    updater = Updater(token='5823371420:AAERluGPDzcPUjQzGnRe9OoBbECe19_JFXk')
+    updater = Updater(token='6526883785:AAEAGc396CqAuokk5o237ZP4k6dIhB0d6_k')
 
     dispatcher = updater.dispatcher
 
