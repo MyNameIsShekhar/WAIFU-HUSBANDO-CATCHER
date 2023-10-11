7bbded94c0f11166a16721f0596422cf11a3165e
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-11 19:05:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -371,9 +371,15 @@ def harrem(update: Update, context: CallbackContext) -> None:
 def inlinequery(update: Update, context: CallbackContext) -> None:
     """Handle the inline query."""
     query = update.inline_query.query
+    if query:
+        matching_characters = list(collection.find({'name': {'$regex': query, '$options': 'i'}}))
+        results = []
+        for character in matching_characters:
+            return
+            
 
     # Check if the query is a user id
-    if query.isdigit():
+    elif query.isdigit():
         # Find the user in the database
         user = user_collection.find_one({'id': int(query)})
 
