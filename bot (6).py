1731f84057c2d05b8f2f477e78aa9ad0304309ac
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-12 11:25:51 +0530

    Update bot (6).py

--- bot (6).py
+++ bot (6).py
@@ -1,7 +1,14 @@
+
 from telethon import TelegramClient, events
-from telegraph import Telegraph, upload_file
-import os
-import requests
+from telegraph import Telegraph
+from pymongo import MongoClient
+import random
+import string
+
+# Initialize MongoDB
+client =MongoClient('mongodb+srv://shuyaaaaa12:NvpoBuRp7MVPcAYA@cluster0.q2yycqx.mongodb.net/')
+db = client['anime_db']
+collection = db['anime_collection']
 
 api_id = '24427150'
 api_hash = '9fcc60263a946ef550d11406667404fa'
@@ -11,82 +18,34 @@ dp = Dispatcher(bot)
 telegraph = Telegraph()
 telegraph.create_account(short_name='anime')
 
-client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+
+@bot.on(events.NewMessage(pattern='/upload'))
+async def upload_handler(event):
+    if event.photo and event.message.split():
+        # Generate unique character id
+        while True:
+            character_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+            if collection.find_one({'character_id': character_id}) is None:
+                break
+
+        # Upload photo to Telegraph and store in DB
+        character_name, anime_name = event.message.split('-')
+        response = telegraph.upload(event.photo)
+        collection.insert_one({
+            'character_id': character_id,
+            'character_name': character_name,
+            'anime_name': anime_name,
+            'photo_url': response[0],
+            'added_by': event.sender_id,
+        })
+
+        # Send message to channel with character info
+        await bot.send_message(-1001670772912, f"New character added!\\nCharacter ID: {character_id}\\nCharacter Name: {character_name}\\nAnime Name: {anime_name}\\nAdded by: {event.sender.first_name}")
+    else:
+        await bot.send_message(event.chat_id, "Incorrect format. Please use /upload with a photo and the format 'character-name-anime-name'.")
 
 
-async def get_next_sequence_number(sequence_name):
-    # Get a handle to the sequence collection
-    sequence_collection = db.sequences
 
-    # Use find_one_and_update to atomically increment the sequence number
-    sequence_document = await sequence_collection.find_one_and_update(
-        {'_id': sequence_name}, 
-        {'$inc': {'sequence_value': 1}}, 
-        return_document=ReturnDocument.AFTER
-    )
 
-    # If this sequence doesn't exist yet, create it
-    if not sequence_document:
-        await sequence_collection.insert_one({'_id': sequence_name, 'sequence_value': 0})
-        return 0
+bot.run_until_disconnected()
 
-    return sequence_document['sequence_value']
-
-@client.on(events.NewMessage(pattern='/upload'))
-async def upload(event):
-    # Check if user is a sudo user
-    if str(event.message.sender_id) not in sudo_users:
-        await event.reply('You do not have permission to use this command.')
-        return
-
-    try:
-        # Extract arguments
-        args = event.message.text.split()[1:]
-        if len(args) != 2:
-            await event.reply('Incorrect format. Please reply to an image with: /upload Character-Name Anime-Name')
-            return
-
-        # Check if replied to a photo
-        if not event.message.is_reply or 'photo' not in event.message.reply_to_message.media:
-            await event.reply('Please reply to a photo.')
-            return
-
-        # Replace '-' with ' ' in character name and convert to title case
-        character_name = args[0].replace('-', ' ').title()
-        anime = args[1].replace('-', ' ').title()
-
-        # Download image
-        photo_path = await event.message.reply_to_message.download_media()
-
-        # Upload image to telegraph
-        response = upload_file(photo_path)
-
-        # Delete downloaded image
-        os.remove(photo_path)
-
-        # Generate ID
-        id = str(await get_next_sequence_number('character_id')).zfill(4)
-
-        # Insert new character
-        character = {
-            'img_url': 'https://telegra.ph' + response[0],
-            'name': character_name,
-            'anime': anime,
-            'id': id
-        }
-
-        collection.insert_one(character)
-
-        # Send image with caption to group
-        await client.send_file(
-            'your_group_id',
-            file=character['img_url'],
-            caption=f'<b>Character Name:</b> {character_name}\n<b>Anime Name:</b> {anime}\n<b>ID:</b> {id}',
-            parse_mode='html'
-        )
-
-        await event.reply('Successfully uploaded.')
-    except Exception as e:
-        await event.reply('Unsuccessfully uploaded.')
-
-client.run_until_disconnected()
