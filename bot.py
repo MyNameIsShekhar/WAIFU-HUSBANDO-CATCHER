b0221f5af38a5bf66dd068fc3f78970c21842d09
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-13 09:05:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,100 +1,53 @@
-import asyncio
+
 from pyrogram import Client, filters
+from telegraph import upload_file
 from pymongo import MongoClient
-import urllib.request
-
-sudo_users = [565452393, 6194728132, 6404226395] 
-
-client = MongoClient('mongodb+srv://shekharhatture:kUi2wj2wKxyUbbG1@cluster0.od4v7eo.mongodb.net/?retryWrites=true&w=majority')
-
-db = client['lmaoooo']
-collection = db['collectionname']
+from bson.objectid import ObjectId
+import os
 
 
-api_id = '24427150'
-api_hash = '9fcc60263a946ef550d11406667404fa'
-bot_token = '6656458442:AAGJ1nKC2qil9SMU3NbElluHSmHJrN8oZsg'
+api_id = 'your_api_id'
+api_hash = 'your_api_hash'
+bot_token = 'your_bot_token'
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 
+db = MongoClient('mongodb+srv://shekharhatture:kUi2wj2wKxyUbbG1@cluster0.od4v7eo.mongodb.net/?retryWrites=true&w=majority')['your_database_name']['your_collection_name']
 
-@app.on_message(filters.command("upload"))
+@app.on_message(filters.command("upload") & filters.reply & filters.photo)
 async def upload(client, message):
-    # Check if user is a sudo user
-    if int(message.from_user.id) not in sudo_users:
-        await message.reply_text('You do not have permission to use this command.')
+    if len(message.command) < 3:
+        await message.reply_text("Please provide both character name and anime name.")
         return
 
-    try:
-        # Extract arguments
-        args = message.command[1:]
-        if len(args) != 3:
-            await message.reply_text('Incorrect format. Please use: /upload img_url Character-Name Anime-Name')
-            return
+    character_name = message.command[1].replace("-", " ")
+    anime = message.command[2].replace("-", " ")
 
-        # Replace '-' with ' ' in character name and convert to title case
-        character_name = args[1].replace('-', ' ').title()
-        anime = args[2].replace('-', ' ').title()
+    photo = await app.download_media(message=message.reply_to_message, file_name="temp.jpg")
+    telegraph_url = upload_file(photo)[0]
+    os.remove(photo)
 
-        # Check if image URL is valid
-        try:
-            urllib.request.urlopen(args[0])
-        except:
-            await message.reply_text('Invalid image URL.')
-            return
+    unique_id = str(ObjectId())
+    data = {
+        "_id": unique_id,
+        "character_name": character_name,
+        "anime": anime,
+        "url": telegraph_url
+    }
 
-        # Generate ID
-        id = str(get_next_sequence_number('character_id')).zfill(4)
+    if db.find_one({"anime": anime}):
+        db.update_one({"anime": anime}, {"$push": {"characters": data}})
+    else:
+        db.insert_one(data)
 
-        # Insert new character
-        character = {
-            'img_url': args[0],
-            'name': character_name,
-            'anime': anime,
-            'id': id
-        }
-        
-        # Send message to channel
-        sent_message = await client.send_photo(
-            chat_id='-1001670772912',
-            photo=args[0],
-            caption=f'<b>Character Name:</b> {character_name}\n<b>Anime Name:</b> {anime}\n<b>ID:</b> {id}\nAdded by <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>',
-            parse_mode='HTML'
-        )
-
-        # Save message_id to character
-        character['message_id'] = sent_message.message_id
-        collection.insert_one(character)
-
-        await message.reply_text('Successfully uploaded.')
-    except Exception as e:
-        await message.reply_text('Unsuccessfully uploaded.')
-
-@app.on_message(filters.command("delete"))
-async def delete(client, message):
-    # Check if user is a sudo user
-    if str(message.from_user.id) not in sudo_users:
-        await message.reply_text('You do not have permission to use this command.')
-        return
-
-    try:
-        # Extract arguments
-        args = message.command[1:]
-        if len(args) != 1:
-            await message.reply_text('Incorrect format. Please use: /delete ID')
-            return
-
-        # Delete character with given ID
-        character = collection.find_one_and_delete({'id': args[0]})
-
-        if character:
-            # Delete message from channel
-            await client.delete_messages(chat_id='-1001670772912', message_ids=character['message_id'])
-            await message.reply_text('Successfully deleted.')
-        else:
-            await message.reply_text('No character found with given ID.')
-    except Exception as e:
-        await message.reply_text('Failed to delete character.')
+    await app.send_photo(
+        chat_id='-1001670772912',
+        photo=telegraph_url,
+        caption=f'<b>Character Name:</b> {character_name}\n<b>Anime Name:</b> {anime}\n<b>ID:</b> {unique_id}\nAdded by <a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a>',
+        parse_mode='HTML'
+    )
 
 app.run()
+
+
