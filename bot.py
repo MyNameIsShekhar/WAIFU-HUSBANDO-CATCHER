f426932e73c205dc042e4b1ca28e4527273e9d7d
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-13 14:36:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,10 +31,8 @@ async def upload_handler(_, message):
             # Check if the URL is valid
             if re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', img_url):
                 # Generate unique ID for the character
-                last_doc = collection.find().sort([('character_id', -1)]).limit(1)
-                last_id = int(last_doc[0]['character_id'])
-                character_id = str(last_id + 1).zfill(4)
-
+                character_id = str(collection.count_documents({}) + 1).zfill(4)
+            
 
                 # Insert into MongoDB
                 character = {
@@ -70,6 +68,27 @@ async def upload_handler(_, message):
             await message.reply_text("Incorrect format. Use /upload img_url anime-name character-name")
     else:
         await message.reply_text("Only sudo users can use this command.")
+
+@app.on_message(filters.command("delete"))
+async def delete_handler(_, message):
+    if message.from_user.id in sudo_users:
+        character_id = message.text.split(' ')[1]
+        character = collection.find_one({'id': character_id})
+        if character:
+            # Delete from MongoDB
+            collection.delete_one({'id': character_id})
+            
+            # Delete from channel
+            await app.delete_messages(channel_id, character['message_id'])
+            
+            await message.reply_text(f"{character['character_name']} deleted successfully.")
+        else:
+            await message.reply_text("Character not found.")
+    else:
+        await message.reply_text("Only sudo users can use this command.")
+                          
+                    
+                    
 
 
 app.run()
