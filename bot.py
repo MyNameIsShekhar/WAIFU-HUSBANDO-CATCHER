a52cb6f2e286a7b7add2e115f213da5c75515d6f
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-13 13:55:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
+import asyncio
 from pyrogram import Client, filters
-from telegraph import Telegraph, upload_file
 from pymongo import MongoClient
 import requests
 import re
 import os
+import uuid
 
 # Connect to MongoDB
-client = MongoClient('mongodb+srv://shekharhatture:kUi2wj2wKxyUbbG1@cluster0.od4v7eo.mongodb.net/?retryWrites=true&w=majority')
-db = client['animeDB']
-collection = db['characters']
+client = MongoClient('mongodb+srv://animedatabaseee:BFm9zcCex7a94Vuj@cluster0.zyi6hqg.mongodb.net/?retryWrites=true&w=majority')
+db = client['Waifus']
+collection = db['anime_characters']
+user_collection = db["user_collection"]
+
 
 # Initialize Pyrogram Client
 api_id = '24427150'
@@ -16,64 +19,60 @@ api_hash = '9fcc60263a946ef550d11406667404fa'
 bot_token = '6430015242:AAG5eGK4MYd9-58PjYfJZy0LhcfMvpWly1I'
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-# Initialize Telegraph Client
-lol = Telegraph()
-lol.create_account(short_name='Anime')
-
 sudo_users = [6404226395]  # Add more sudo user IDs to this list if needed
-channel_id = -1001670772912
 
-@app.on_message(filters.photo & filters.command("upload"))
+@app.on_message(filters.command("upload"))
 async def upload_handler(_, message):
     # Check if the user is a sudo user
     if message.from_user.id in sudo_users:
-        msg = message.caption.split(' ')
-        if len(msg) == 2:
-            anime_name, character_name = msg[0].replace('-', ' ').title(), msg[1].replace('-', ' ').title()
+        msg = message.text.split(' ')
+        if len(msg) == 4:
+            img_url, anime_name, character_name = msg[1], msg[2].replace('-', ' ').title(), msg[3].replace('-', ' ').title()
+            # Check if the URL is valid
+            if re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', img_url):
+                # Generate unique ID for the character
+                character_id = str(collection.count_documents({}) + 1).zfill(4)
             
-            # Download image
-            img_url = message.photo.file_path
-            response = requests.get(img_url)
-            file_name = os.path.join('/tmp', 'image.jpg')
-            with open(file_name, 'wb') as f:
-                f.write(response.content)
-            
-            # Upload image to Telegraph
-            telegraph_url = 'https://telegra.ph' + upload_file(file_name)[0]
-            
-            # Generate unique ID for the character
-            character_id = str(collection.count_documents({}) + 1).zfill(4)
-            
-            # Insert into MongoDB
-            character_doc = {
-                "id": character_id,
-                "anime_name": anime_name,
-                "character_name": character_name,
-                "img_url": telegraph_url,
-                "added_by": message.from_user.id
-            }
-            collection.insert_one(character_doc)
-            await message.reply_text("Successfully uploaded.")
-            
-            # Send to channel
-              # Replace with your channel ID
-            caption = f'{message.from_user.mention} added a new character:\n\n' \
-                      f'**ID**: {character_id}\n' \
-                      f'**Anime**: {anime_name}\n' \
-                      f'**Character**: {character_name}'
-            await app.send_photo(channel_id, file_name, caption=caption)
-            
-            # Delete image file
-            os.remove(file_name)
+                # Insert into MongoDB
+                character = {
+                    "id": character_id,
+                    "anime_name": anime_name,
+                    "character_name": character_name,
+                    "img_url": img_url,
+                    "added_by": message.from_user.id
+                }
+                collection.insert_one(character)
+                await message.reply_text("Successfully uploaded.")
+                
+                # Download image
+                response = requests.get(img_url)
+                file_name = os.path.join('/tmp', 'image.jpg')
+                with open(file_name, 'wb') as f:
+                    f.write(response.content)
+                
+                # Send to channel
+                channel_id = -1001670772912 # Replace with your channel ID
+                caption = f'{message.from_user.mention} added a new character:\n\n' \
+                          f'**ID**: {character_id}\n' \
+                          f'**Anime**: {anime_name}\n' \
+                          f'**Character**: {character_name}'
+                await app.send_photo(channel_id, file_name, caption=caption)
+                
+                # Delete image file
+                os.remove(file_name)
+            else:
+                await message.reply_text("Invalid image URL.")
         else:
-            await message.reply_text("Incorrect format. Send photo with caption: /upload anime-name character-name")
+            await message.reply_text("Incorrect format. Use /upload img_url anime-name character-name")
     else:
         await message.reply_text("Only sudo users can use this command.")
 
-
 def main():
-    app.run()
+    asyncio.run(app.run())
 
 if __name__ == "__main__":
     main()
 
+                          
+                    
+                    
