fa1e69372bf7844b100a7a3a187639423fbf8506
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-13 14:48:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,21 +72,27 @@ async def upload_handler(_, message):
 @app.on_message(filters.command("delete"))
 async def delete_handler(_, message):
     if message.from_user.id in sudo_users:
-        character_id = message.text.split(' ')[1]
+        msg = message.text.split(' ')
+        if len(msg) < 2:
+            await message.reply_text("Please provide a character ID. Use /delete character_id")
+            return
+
+        character_id = msg[1]
         character = collection.find_one({'id': character_id})
         if character:
             # Delete from MongoDB
             collection.delete_one({'id': character_id})
             
             # Delete from channel
-            await app.delete_messages(channel_id, character['message_id'])
+            if 'message_id' in character:
+                await app.delete_messages(channel_id, character['message_id'])
             
             await message.reply_text(f"{character['character_name']} deleted successfully.")
         else:
             await message.reply_text("Character not found.")
     else:
         await message.reply_text("Only sudo users can use this command.")
-                          
+
                     
                     
 
