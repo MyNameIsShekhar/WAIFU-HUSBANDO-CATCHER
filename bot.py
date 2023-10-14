e5c3eae2229d00fffee8aacd299026351855b67e
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-14 08:39:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ db = client['anime_db']
 collection = db['anime_collection']
 
 CHANNEL_ID = -1001683394959
-SUDO_USER_ID = ['6404226395']
+SUDO_USER_ID = [6404226395]
 async def generate_id():
     for i in range(1, 10000):
         id = str(i).zfill(4)
@@ -31,7 +31,7 @@ async def is_url_valid(url):
 
 @dp.message_handler(commands=['upload'])
 async def upload(message: types.Message):
-    if int(message.from_user.id) == SUDO_USER_ID:
+    if int(message.from_user.id) in SUDO_USER_ID:
         try:
             _, img_url, anime_name, character_name = message.text.split(' ')
             anime_name = anime_name.replace('-', ' ')
