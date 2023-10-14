63932f8c95f8f4970aa406e8b46c303fda135f31
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-14 09:13:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ async def ping(message: types.Message):
 
 @dp.message_handler(commands=['upload'])
 async def upload(message: types.Message):
-    if message.from_user.id == SUDO_USER_ID:
+    if message.from_user.id in SUDO_USER_ID:
         try:
             _, img_url, anime_name, character_name = message.text.split(' ')
             character_name = character_name.replace('-', ' ')
