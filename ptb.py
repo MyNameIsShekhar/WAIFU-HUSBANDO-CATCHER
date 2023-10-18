752afd64d9c2d376367d76f4b3fc2594b50897bd
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-18 14:43:58 +0530

    Update ptb.py

--- ptb.py
+++ ptb.py
@@ -1,15 +1,1 @@
-import telegram.ext as tg
 
-WORKERS = (
-        8 # Number of subthreads to use. Set as number of threads your processor uses
-)
-TOKEN = "6420751168:AAEtf-OyEYLLTZM2c4LrhIroXPfvsW7KlM8"
-
-updater = tg.Updater(
-    token=TOKEN,
-    workers=WORKERS,
-    request_kwargs={"read_timeout": 10, "connect_timeout": 10},
-    use_context=True,
-)
-
-dispatcher = updater.dispatcher
