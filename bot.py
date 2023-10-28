5a9b10d02d658f6bde10040b4d04a7d6ed66492f
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-28 08:21:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,93 +1,126 @@
-from telethon import TelegramClient, events
-from motor.motor_asyncio import AsyncIOMotorClient
+import importlibfrom itertools import groupby
+from telegram import InputMediaPhoto
+from telegram import Update
+from motor.motor_asyncio import AsyncIOMotorClient 
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, filters, ApplicationBuilder 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultPhoto, InputTextMessageContent, InputMediaPhoto
+from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import InlineQueryHandler,CallbackQueryHandler, ChosenInlineResultHandler
+from pymongo import MongoClient, ReturnDocument
+import urllib.request
 import random
-import asyncio
+from datetime import datetime, timedelta
+from threading import Lock
+import time
 
-# Create instances of TelegramClient and Motor's AsyncIOMotorClient
-
-api_id = '24427150'
-api_hash = '9fcc60263a946ef550d11406667404fa'
-bot_token = '6794007851:AAFOj7qlMXlyRU4GlNqjVtO-5snYvu6-cwc'
-
-app = TelegramClient('my_session', api_id, api_hash)
-app.start(bot_token=bot_token)
-
+from Botttt.modules import ALL_MODULES
 client = AsyncIOMotorClient('mongodb+srv://animedatabaseee:BFm9zcCex7a94Vuj@cluster0.zyi6hqg.mongodb.net/?retryWrites=true&w=majority')
 db = client['Waifus_lol']
-#Your collections
-user_totals_collection = db['user_totals_lmaoooo']
 collection = db['anime_characters_lol']
+
+# Get the collection for user totals
+user_totals_collection = db['user_totals_lmaoooo']
+user_collection = db["user_collection_lmaoooo"]
+
+group_user_totals_collection = db['group_user_totalssssss']
+
+
+# List of sudo users
+sudo_users = ['6404226395', '6185531116', '5298587903', '5798995982', '5150644651', '5813998595', '5813403535', '6393627898', '5952787198', '6614280216','6248411931','5216262234','1608353423']
+
+
+# Create a dictionary of locks
 locks = {}
-# Your other global variables here...
+# Counter for messages in each group
+message_counters = {}
+spam_counters = {}
+# Last sent character in each group
+last_characters = {}
 
-@app.on(events.NewMessage())
-async def message_counter(event):
-    chat_id = str(event.chat_id)
+# Characters that have been sent in each group
+sent_characters = {}
 
-    # Get or create a lock for this chat
-    if chat_id not in locks:
-        locks[chat_id] = asyncio.Lock()
-    lock = locks[chat_id]
+# Keep track of the user who guessed correctly first in each group
+first_correct_guesses = {}
 
-    # Use the lock to ensure that only one instance of this function can run at a time for this chat
-    async with lock:
-        # Get message frequency and counter for this chat from the database
-        chat_frequency = await user_totals_collection.find_one({'chat_id': chat_id})
-        if chat_frequency:
-            message_frequency = chat_frequency.get('message_frequency', 10)
-            message_counter = chat_frequency.get('message_counter', 0)
-        else:
-            # Default to 20 messages if not set
-            message_frequency =10
-            message_counter = 0
+# Import asyncio
+import asyncio
 
-        # Increment counter for this chat
-        message_counter += 1
+# Convert your function to async
+async def get_next_sequence_number(sequence_name):
+    # Get a handle to the sequence collection
+    sequence_collection = db.sequences
 
-        # Send image after every message_frequency messages
-        if message_counter % message_frequency == 0:
-            await send_image(event)
-            # Reset counter for this chat
-            message_counter = 0
-
-        # Update counter in the database
-        await user_totals_collection.update_one(
-            {'chat_id': chat_id},
-            {'$set': {'message_counter': message_counter}},
-            upsert=True
-        )
-
-async def send_image(event):
-    chat_id = event.chat_id
-
-    # Get all characters
-    all_characters = [doc async for doc in collection.find({})]
-    
-    # Initialize sent characters list for this chat if it doesn't exist
-    if chat_id not in sent_characters:
-        sent_characters[chat_id] = []
-
-    # Reset sent characters list if all characters have been sent
-    if len(sent_characters[chat_id]) == len(all_characters):
-        sent_characters[chat_id] = []
-
-    # Select a random character that hasn't been sent yet
-    character = random.choice([c for c in all_characters if c['id'] not in sent_characters[chat_id]])
-
-    # Add character to sent characters list and set as last sent character
-    sent_characters[chat_id].append(character['id'])
-    last_characters[chat_id] = character
-
-    # Reset first correct guess when a new character is sent
-    if chat_id in first_correct_guesses:
-        del first_correct_guesses[chat_id]
-
-    # Send image with caption
-    await app.send_file(
-        event.chat_id,
-        file=character['img_url'],
-        caption="Use /Guess Command And.. Guess This Character Name.."
+    # Use find_one_and_update to atomically increment the sequence number
+    sequence_document = await sequence_collection.find_one_and_update(
+        {'_id': sequence_name}, 
+        {'$inc': {'sequence_value': 1}}, 
+        return_document=ReturnDocument.AFTER
     )
 
+    # If this sequence doesn't exist yet, create it
+    if not sequence_document:
+        await sequence_collection.insert_one({'_id': sequence_name, 'sequence_value': 0})
+        return 0
 
-app.run_until_disconnected()
+    return sequence_document['sequence_value']
+
+async def upload(update: Update, context: CallbackContext) -> None:
+    # Check if user is a sudo user
+    if str(update.effective_user.id) not in sudo_users:
+        await update.message.reply_text('You do not have permission to use this command.')
+        return
+
+    try:
+        # Extract arguments
+        args = context.args
+        if len(args) != 4:
+            await update.message.reply_text('Incorrect format. Please use: /upload img_url Character-Name Anime-Name Rarity')
+            return
+
+        # Replace '-' with ' ' in character name and convert to title case
+        character_name = args[1].replace('-', ' ').title()
+        anime = args[2].replace('-', ' ').title()
+
+        # Check if image URL is valid
+        try:
+            urllib.request.urlopen(args[0])
+        except:
+            await update.message.reply_text('Invalid image URL.')
+            return
+
+        # Check if rarity is valid
+        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium"}
+        try:
+            rarity = rarity_map[int(args[3])]
+        except KeyError:
+            await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, or 4.')
+            return
+
+        # Generate ID
+        id = str(await get_next_sequence_number('character_id')).zfill(4)
+
+        # Insert new character
+        character = {
+            'img_url': args[0],
+            'name': character_name,
+            'anime': anime,
+            'rarity': rarity,
+            'id': id
+        }
+        
+        # Send message to channel
+        message = await context.bot.send_photo(
+            chat_id='-1001915956222',
+            photo=args[0],
+            caption=f'<b>Character Name:</b> {character_name}\n<b>Anime Name:</b> {anime}\n<b>Rarity:</b> {rarity}\n<b>ID:</b> {id}\nAdded by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+            parse_mode='HTML'
+        )
+
+        # Save message_id to character
+        character['message_id'] = message.message_id
+        await collection.insert_one(character)
+
+        await update.message.reply_text('Successfully uploaded.')
+    except Exception as e:
+        await update.message.reply_text('Unsuccessfully uploaded.')
