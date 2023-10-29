c4be7ef12ea192e2abaae5238a2d8918179d5c6e
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-30 01:04:56 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -22,6 +22,6 @@ TOKEN = "6420751168:AAEtf-OyEYLLTZM2c4LrhIroXPfvsW7KlM8"
 application = Application.builder().token(TOKEN).build()
 
 client = AsyncIOMotorClient('mongodb+srv://shuyaashivu:9fcc60263a946ef550d11406667404fa@cluster0.ikub9lo.mongodb.net/?retryWrites=true&w=majority')
-db = client['Character_catcherr']
+db = client['Character_catcher']
 
 sudo_users = ['6404226395', '6185531116', '5298587903', '5798995982', '5150644651','5813403535', '6393627898', '5952787198', '6614280216','6248411931','5216262234','1608353423']
