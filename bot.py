26ee2cd97585d03190b960d37f8576da463c991c
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-29 06:33:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -398,7 +398,7 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
                             thumbnail_url=character['img_url'],
                             id=character['id'],
                             photo_url=character['img_url'],
-                            caption=f"🌻 <b><a href='tg://user?id={user['id']}'>{user.get('first_name', user['id'])}</a>'s Character</b>'\n\n<b>⟹ {character['name']}</b> " + (f"<b>(x{character.get('count', 1)})</b>" if character.get('count', 1)  + f"\n<b>⟹ {character['anime']} ({anime_characters_guessed}/{total_anime_characters})</b>\n\n<b>Rarity: {rarity}</b>\n<b>🆔: {character['id']}</b>",
+                            caption=f"🌻 <b><a href='tg://user?id={user['id']}'>{user.get('first_name', user['id'])}</a>'s Character</b>'\n\n<b>⟹ {character['name']}</b> " + (f"<b>(x{character.get('count', 1)}))</b>" if character.get('count', 1)  + f"\n<b>⟹ {character['anime']} ({anime_characters_guessed}/{total_anime_characters})</b>\n\n<b>Rarity: {rarity}</b>\n<b>🆔: {character['id']}</b>",
                             parse_mode='HTML'
                     )
                     )
