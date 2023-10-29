7adbc13dd6f60172ff2623498232aacfc4a044ef
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-30 03:21:37 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -16,6 +16,7 @@ collection = db['your_collection_name']  # replace with your collection name
 
 photo_url_list = ["https://graph.org/file/38767e79402baa8b04125.jpg"]  # replace with your list of photo URLs
 
+
 async def start(update: Update, context: CallbackContext) -> None:
     if update.effective_chat.type != "private":
         return
@@ -39,17 +40,6 @@ async def start(update: Update, context: CallbackContext) -> None:
     photo_url = random.choice(photo_url_list)
     
     
-    start_keyboard = [
-        [InlineKeyboardButton("Help", callback_data='help')],
-        [InlineKeyboardButton("Support", url=f'https://t.me/collect_em_all')],
-    ]
-    
-    reply_markup = InlineKeyboardMarkup(start_keyboard)
-    
-    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption=caption, reply_markup=reply_markup)
-    
-    elif query.data == 'back':
-    
     keyboard = [
         [InlineKeyboardButton("Help", callback_data='help')],
         [InlineKeyboardButton("Support", url=f'https://t.me/collect_em_all')],
@@ -57,7 +47,7 @@ async def start(update: Update, context: CallbackContext) -> None:
     
     reply_markup = InlineKeyboardMarkup(keyboard)
     
-    await context.bot.edit_message_caption(chat_id=update.effective_chat.id, message_id=query.message.message_id, caption=caption, reply_markup=reply_markup)
+    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption=caption, reply_markup=reply_markup)
 
 
 async def button(update: Update, context: CallbackContext) -> None:
@@ -66,16 +56,17 @@ async def button(update: Update, context: CallbackContext) -> None:
 
     if query.data == 'help':
         help_text = """
-    ***Help Section :***
-    
-***/guess:*** To Guess character (only works in group)
-***/fav:*** Add Your fav
-***/give:*** Give any Character from Your Collection to another user.. (only works in groups)
-***/collection:*** to see Your Collection
-***/grouptop:*** See Top Group users in group (only Works in Groups)
-***/globaltop:*** To See Top Global Users
-***/changetime:*** Change Character appear time (only works in Groups)
-   """
+        Help Section : 
+
+        /guess: To Guess character (only works in group)
+        /fav: Add Your fav
+        /give: Give any Character from Your Collection to another user.. (only works in groups)
+        /collection: to see Your Collection 
+        /grouptop: See Top Group users in group (only Works in Groups)
+        /globaltop: To See Top Global Users
+        /changetime: Change Character appear time (only works in Groups)
+
+        """
         keyboard = [[InlineKeyboardButton("Back", callback_data='back')]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         
@@ -95,9 +86,6 @@ async def button(update: Update, context: CallbackContext) -> None:
         await context.bot.edit_message_caption(chat_id=update.effective_chat.id, message_id=query.message.message_id, caption=caption, reply_markup=reply_markup)
 
 
-        
-    
-    
 
 button_handler = CallbackQueryHandler(button)
 application.add_handler(button_handler)
