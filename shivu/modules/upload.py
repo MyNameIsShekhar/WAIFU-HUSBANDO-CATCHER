d956db52e1774ee65db5a57a022d0128bf6f688e
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-29 11:11:39 +0530

    Create upload.py

---
+++ shivu/modules/upload.py
@@ -0,0 +1,91 @@
+from telegram import Update
+from motor.motor_asyncio import AsyncIOMotorClient 
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, filters, Application
+from telegram.ext import InlineQueryHandler,CallbackQueryHandler, ChosenInlineResultHandler
+from pymongo import MongoClient, ReturnDocument
+import urllib.request
+import random
+from shivu import application 
+from shivu import db
+
+collection = db['anime_characters_lol']
+
+def get_next_sequence_number(sequence_name):
+    # Get a handle to the sequence collection
+    sequence_collection = db.sequences
+
+    # Use find_one_and_update to atomically increment the sequence number
+    sequence_document = sequence_collection.find_one_and_update(
+        {'_id': sequence_name}, 
+        {'$inc': {'sequence_value': 1}}, 
+        return_document=ReturnDocument.AFTER
+    )
+
+    # If this sequence doesn't exist yet, create it
+    if not sequence_document:
+        sequence_collection.insert_one({'_id': sequence_name, 'sequence_value': 0})
+        return 0
+
+    return sequence_document['sequence_value']
+
+def upload(update: Update, context: CallbackContext) -> None:
+    # Check if user is a sudo user
+    if str(update.effective_user.id) not in sudo_users:
+        update.message.reply_text('You do not have permission to use this command.')
+        return
+
+    try:
+        # Extract arguments
+        args = context.args
+        if len(args) != 4:
+            update.message.reply_text('Incorrect format. Please use: /upload img_url Character-Name Anime-Name Rarity')
+            return
+
+        # Replace '-' with ' ' in character name and convert to title case
+        character_name = args[1].replace('-', ' ').title()
+        anime = args[2].replace('-', ' ').title()
+
+        
+        try:
+            urllib.request.urlopen(args[0])
+        except:
+            update.message.reply_text('Invalid image URL.')
+            return
+
+        # rarity
+        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium"}
+        try:
+            rarity = rarity_map[int(args[3])]
+        except KeyError:
+            update.message.reply_text('Invalid rarity. Please use 1, 2, 3, or 4.')
+            return
+
+        
+        id = str(get_next_sequence_number('character_id')).zfill(4)
+
+        
+        character = {
+            'img_url': args[0],
+            'name': character_name,
+            'anime': anime,
+            'rarity': rarity,
+            'id': id
+        }
+        
+        
+        message = context.bot.send_photo(
+            chat_id='-1001915956222',
+            photo=args[0],
+            caption=f'<b>Character Name:</b> {character_name}\n<b>Anime Name:</b> {anime}\n<b>Rarity:</b> {rarity}\n<b>ID:</b> {id}\nAdded by <a href="tg://user?id={update.effective_user.id}">{update.effective_user.first_name}</a>',
+            parse_mode='HTML'
+        )
+
+        
+        character['message_id'] = message.message_id
+        collection.insert_one(character)
+
+        update.message.reply_text('✅️ Done...Cheack in @characters_database')
+    except Exception as e:
+        update.message.reply_text('Unsuccessfully uploaded')
+      
+application.add_handler(CommandHandler("upload", upload)  
