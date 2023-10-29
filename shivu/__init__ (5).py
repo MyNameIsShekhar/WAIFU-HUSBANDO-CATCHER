37c8c4e8ccea095d3c57a9d88e8dc1fdff0a998c
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-29 10:12:13 +0530

    Update __init__ (5).py

--- shivu/__init__ (5).py
+++ shivu/__init__ (5).py
@@ -1,9 +1,7 @@
 import telegram.ext as tg
 import logging 
 
-WORKERS = (
-        8 # Number of subthreads to use. Set as number of threads your processor uses
-)
+
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
@@ -16,12 +14,5 @@ logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 TOKEN = "6420751168:AAEtf-OyEYLLTZM2c4LrhIroXPfvsW7KlM8"
-updater = tg.Updater(
-    token=TOKEN,
-    workers=WORKERS,
-    request_kwargs={"read_timeout": 10, "connect_timeout": 10},
-    use_context=True,
-)
 
-dispatcher = updater.dispatcher
 
