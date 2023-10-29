ca33fec7801e7dad8ee651c0045511bc4fcdfbf5
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-29 09:01:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,14 +61,14 @@ async def message_counter(update: Update, context: CallbackContext) -> None:
         # Get message frequency and counter for this chat from the database
         chat_frequency = await user_totals_collection.find_one({'chat_id': chat_id})
         if chat_frequency:
-            message_frequency = chat_frequency.get('message_frequency', 10)
+            message_frequency = chat_frequency.get('message_frequency', 100)
             message_counter = chat_frequency.get('message_counter', 0)
         else:
-            # Default to 20 messages if not set
-            message_frequency =10
+        
+            message_frequency =100
             message_counter = 0
 
-        # Increment counter for this chat
+        
         message_counter += 1
 
         # Send image after every message_frequency messages
