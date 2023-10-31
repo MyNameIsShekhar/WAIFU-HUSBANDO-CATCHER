229aa6a6f1c003b1fa19090d29568d98e150b042
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-31 12:57:27 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -28,7 +28,7 @@ async def start(update: Update, context: CallbackContext) -> None:
         ***Hey there! {update.effective_user.first_name} 🌻***
         
         
-        i Am Collect 'Em All Bot.. Add Me in You're Group And I will send Random Characters in group after every 100 messages and who guessed that character's name Correct.. I will add That Character in That user's Collection.. Tap on help Button To See All Commands
+i Am Collect 'Em All Bot.. Add Me in You're Group And I will send Random Characters in group after every 100 messages and who guessed that character's name Correct.. I will add That Character in That user's Collection.. Tap on help Button To See All Commands
         """
     keyboard = [
             [InlineKeyboardButton("Add Me", url=f'http://t.me/Collect_emAll_Bot?startgroup=new')],
@@ -40,7 +40,7 @@ async def start(update: Update, context: CallbackContext) -> None:
         ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     
-    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption=caption, reply_markup=reply_markup)
+    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption=caption, reply_markup=reply_markup, parse_mode='markdown')
 
 
 async def button(update: Update, context: CallbackContext) -> None:
@@ -59,17 +59,17 @@ async def button(update: Update, context: CallbackContext) -> None:
 /globaltop: To See Top Global Users***
 /changetime: Change Character appear time (only works in Groups)
    """
-        keyboard = [[InlineKeyboardButton("Back", callback_data='back')]]
-        reply_markup = InlineKeyboardMarkup(keyboard)
+        help_keyboard = [[InlineKeyboardButton("Back", callback_data='back')]]
+        reply_markup = InlineKeyboardMarkup(help_keyboard)
         
         await context.bot.edit_message_caption(chat_id=update.effective_chat.id, message_id=query.message.message_id, caption=help_text, reply_markup=reply_markup, parse_mode='markdown')
 
     elif query.data == 'back':
         caption = f"""
-        ***Hey there! {update.effective_user.first_name} 🌻***
+        **Hey there! {update.effective_user.first_name} 🌻**
         
         
-        i Am Collect 'Em All Bot.. Add Me in You're Group And I will send Random Characters in group after every 100 messages and who guessed that character's name Correct.. I will add That Character in That user's Collection.. Tap on help Button To See All Commands
+i Am Collect 'Em All Bot.. Add Me in You're Group And I will send Random Characters in group after every 100 messages and who guessed that character's name Correct.. I will add That Character in That user's Collection.. Tap on help Button To See All Commands
         """
         keyboard = [
             [InlineKeyboardButton("Add Me", url=f'http://t.me/Collect_emAll_Bot?startgroup=new')],
@@ -82,7 +82,7 @@ async def button(update: Update, context: CallbackContext) -> None:
         
         reply_markup = InlineKeyboardMarkup(keyboard)
         
-        await context.bot.edit_message_caption(chat_id=update.effective_chat.id, message_id=query.message.message_id, caption=caption, reply_markup=reply_markup)
+        await context.bot.edit_message_caption(chat_id=update.effective_chat.id, message_id=query.message.message_id, caption=caption, reply_markup=reply_markup, parse_mode='markdown')
 
 
 
