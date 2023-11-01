700df611212847604ced0a69dc0299819d2c5886
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-02 01:38:15 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -114,7 +114,7 @@ async def message_counter(update: Update, context: CallbackContext) -> None:
         # Check if the last 6 messages were sent by the same user
         if chat_id in last_user and last_user[chat_id]['user_id'] == user_id:
             last_user[chat_id]['count'] += 1
-            if last_user[chat_id]['count'] >= 10:
+            if last_user[chat_id]['count'] >= 100:
                 # If the user has been warned in the last 10 minutes, ignore their messages
                 if user_id in warned_users and time.time() - warned_users[user_id] < 600:
                     return
@@ -197,8 +197,8 @@ async def guess(update: Update, context: CallbackContext) -> None:
     # Split the character's name into parts by space
     name_parts = last_characters[chat_id]['name'].lower().split()
 
-    # Check if the guess is the full name of the character in any order, or any part of the name exactly
-    if sorted(name_parts) == sorted(guess.split()) or any(part == guess for part in name_parts):
+    # Check if the guess contains all parts of the character's name
+    if all(part in guess for part in name_parts):
         # Rest of the function...
 
     
