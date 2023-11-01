ccac6f63ba37cf3d5bfd73c061569ba1d1e59816
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-01 18:25:56 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -17,11 +17,10 @@ logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 
-TOKEN = "6670374898:AAENkuuAVF6Weivfm4bIw2H5MFcP43FxrbU"
-
+TOKEN = "6420751168:AAG63ZLO8kMgMa6SZCQWjJV0-onZqVsGfsI"
 application = Application.builder().token(TOKEN).build()
 
-client = AsyncIOMotorClient('mongodb+srv://shuyaaaaa12:NvpoBuRp7MVPcAYA@cluster0.q2yycqx.mongodb.net/')
+client = AsyncIOMotorClient('mongodb+srv://shuyaashivu:9fcc60263a946ef550d11406667404fa@cluster0.ikub9lo.mongodb.net/?retryWrites=true&w=majority')
 db = client['Character_catcher']
 
 sudo_users = ['6404226395', '6185531116', '5298587903', '5798995982', '5150644651','5813403535', '6393627898', '5952787198', '6614280216','6248411931','5216262234','1608353423']
