b1c35f2e63920f836a7e57ffae654ca8fc067fa1
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-01 17:58:39 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -389,7 +389,6 @@ def main() -> None:
     application.add_handler(InlineQueryHandler(inlinequery, block=False))
     application.add_handler(CommandHandler('fav', fav, block=False))
     application.add_handler(CommandHandler("collection", harem,block=False))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message, block=False))
     
     application.run_polling( drop_pending_updates=True)
 
