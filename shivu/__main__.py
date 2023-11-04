c0f3b1ad1d3712a1d13f91828ec2201858d6d3cc
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-04 17:01:11 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -410,6 +410,7 @@ async def gift(update: Update, context: CallbackContext) -> None:
 
 
 
+
 async def harem(update: Update, context: CallbackContext, page=0) -> None:
     user_id = update.effective_user.id
 
@@ -470,8 +471,7 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-  else:
-      if 'favorites' in user and user['favorites']:
+    if 'favorites' in user and user['favorites']:
         # Get the favorite character
         fav_character_id = user['favorites'][0]
         fav_character = next((c for c in user['characters'] if c['id'] == fav_character_id), None)
@@ -491,30 +491,29 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
                 if update.callback_query.message.text != harem_message:
                     await update.callback_query.edit_message_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
     else:
-        # Get a random character from the user's collection
-        random_character = random.choice(user['characters'])
+        # Check if the user's collection is not empty
+        if user['characters']:
+            # Get a random character from the user's collection
+            random_character = random.choice(user['characters'])
 
-        if 'img_url' in random_character:
-            if update.message:
-                await update.message.reply_photo(photo=random_character['img_url'], parse_mode='HTML', caption=harem_message, reply_markup=reply_markup)
+            if 'img_url' in random_character:
+                if update.message:
+                    await update.message.reply_photo(photo=random_character['img_url'], parse_mode='HTML', caption=harem_message, reply_markup=reply_markup)
+                else:
+                    # Check if the new caption is different from the existing one
+                    if update.callback_query.message.caption != harem_message:
+                        await update.callback_query.edit_message_caption(caption=harem_message, reply_markup=reply_markup, parse_mode='HTML')
             else:
-                # Check if the new caption is different from the existing one
-                if update.callback_query.message.caption != harem_message:
-                    await update.callback_query.edit_message_caption(caption=harem_message, reply_markup=reply_markup)
+                if update.message:
+                    await update.message.reply_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
+                else:
+                    # Check if the new text is different from the existing one
+                    if update.callback_query.message.text != harem_message:
+                        await update.callback_query.edit_message_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
         else:
             if update.message:
-                await update.message.reply_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
-            else:
-                # Check if the new text is different from the existing one
-                if update.callback_query.message.text != harem_message:
-                    await update.callback_query.edit_message_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
-
-    else:
-        if update.message:
-            await update.message.reply_text("Your list is empty.")
-      
-       
-        
+                await update.message.reply_text("Your list is empty.")
+            
       
       
 
