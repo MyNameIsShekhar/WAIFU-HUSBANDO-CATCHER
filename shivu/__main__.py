f37fb3d702879de5f241fa5894d6fdee585ba6e6
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-04 08:18:20 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -542,7 +542,7 @@ def main() -> None:
     
     
     
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_counter, block=False))
+    application.add_handler(MessageHandler(filters.ANY, message_counter, block=False))
     application.add_handler(CommandHandler(["guess", "protecc", "collect", "grab", "hunt"], guess, block=False))
     application.add_handler(CommandHandler(["changetime"], change_time, block=False))
     application.add_handler(InlineQueryHandler(inlinequery, block=False))
