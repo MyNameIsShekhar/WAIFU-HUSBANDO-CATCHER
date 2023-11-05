ed98e9bc16f8b3ecff0376c63c0cb1210d94adba
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-05 11:40:30 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -74,7 +74,7 @@ async def message_counter(update: Update, context: CallbackContext) -> None:
         # Check if the last 6 messages were sent by the same user
         if chat_id in last_user and last_user[chat_id]['user_id'] == user_id:
             last_user[chat_id]['count'] += 1
-            if last_user[chat_id]['count'] >= 10:
+            if last_user[chat_id]['count'] >= 1000:
                 # If the user has been warned in the last 10 minutes, ignore their messages
                 if user_id in warned_users and time.time() - warned_users[user_id] < 600:
                     return
