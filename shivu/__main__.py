a14d30ca6b3fcb8ed6feb6e89c85cb4819c04600
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-05 19:41:40 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -23,6 +23,7 @@ import html
 from shivu.modules import ALL_MODULES
 from shivu import application 
 from shivu import db
+from shivu import shivuu
 
 collection = db['anime_characters_lol']
 
@@ -34,12 +35,6 @@ group_user_totals_collection = db['group_user_totalsssssss']
 top_global_groups_collection = db['top_global_groups']
 
 
-app = Client(
-    "lmao",
-    api_id="24427150",
-    api_hash="9fcc60263a946ef550d11406667404fa",
-    bot_token="6420751168:AAG63ZLO8kMgMa6SZCQWjJV0-onZqVsGfsI"
-)
 
 
 
@@ -609,69 +604,6 @@ async def harem_callback(update: Update, context: CallbackContext) -> None:
     # Call the harem function with the page number and user_id
     await harem(update, context, page)
 
-
-# This dictionary will hold the trade offers until they are confirmed or cancelled
-pending_trades = {}
-
-@app.on_message(filters.command(["couple", "trade"]))
-async def trade(client, message):
-    sender_id = message.from_user.id
-
-    if not message.reply_to_message:
-        await message.reply_text("You need to reply to a user's message to trade a character!")
-        return
-
-    receiver_id = message.reply_to_message.from_user.id
-
-    if sender_id == receiver_id:
-        await message.reply_text("You can't trade a character with yourself!")
-        return
-
-    if len(message.command) != 3:
-        await message.reply_text("You need to provide two character IDs!")
-        return
-
-    sender_character_id, receiver_character_id = message.command[1], message.command[2]
-
-    # Add the trade offer to the pending trades
-    pending_trades[(sender_id, receiver_id)] = (sender_character_id, receiver_character_id)
-
-    # Create a confirmation button
-    keyboard = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("Confirm Trade", callback_data="confirm_trade")],
-            [InlineKeyboardButton("Cancel Trade", callback_data="cancel_trade")]
-        ]
-    )
-
-    await message.reply_text(f"{message.reply_to_message.from_user.mention}, do you accept this trade?", reply_markup=keyboard)
-
-@app.on_callback_query(filters.create(lambda _, __, query: query.data in ["confirm_trade", "cancel_trade"]))
-async def on_callback_query(client, callback_query):
-    receiver_id = callback_query.from_user.id
-
-    # Find the trade offer
-    for (sender_id, _receiver_id), (sender_character_id, receiver_character_id) in pending_trades.items():
-        if _receiver_id == receiver_id:
-            break
-    else:
-        await callback_query.message.edit_text("You don't have any pending trade offers!")
-        return
-
-    if callback_query.data == "confirm_trade":
-        # Perform the trade
-        # This is where you would add your code to swap the characters in the users' collections
-
-        # Remove the trade offer from the pending trades
-        del pending_trades[(sender_id, receiver_id)]
-
-        await callback_query.message.edit_text(f"You have successfully traded your character with {callback_query.message.reply_to_message.from_user.mention}!")
-
-    elif callback_query.data == "cancel_trade":
-        # Remove the trade offer from the pending trades
-        del pending_trades[(sender_id, receiver_id)]
-
-        await callback_query.message.edit_text("You have successfully cancelled the trade.")
 
 
 # This dictionary will hold the trade offers until they are confirmed or cancelled
