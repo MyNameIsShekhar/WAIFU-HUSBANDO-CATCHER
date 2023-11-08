ca5bf4fd220433c2086988580af0f37e6bcda74b
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-08 09:09:14 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -297,7 +297,7 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
                         thumbnail_url=character['img_url'],
                         id=f"{character['id']}_{time.time()}",
                         photo_url=character['img_url'],
-                        caption=f"🌻 <b><a href='tg://user?id={user['id']}'>{user.get('first_name', user['id'])}</a></b>'s Character\n\n🌸: <b>{character['name']}</b>\n🏖️: <b>{character['anime']} ({anime_characters_guessed}/{total_anime_characters})</b>\n<b>{rarity}</b>\n\n🆔: <b>{character['id']}</b>",
+                        caption=f"🌻 <b><a href='tg://user?id={user['id']}'>{user.get('first_name', user['id'])}</a>'s Character</b>\n\n🌸: <b>{character['name']}</b>\n🏖️: <b>{character['anime']} ({anime_characters_guessed}/{total_anime_characters})</b>\n<b>{rarity}</b>\n\n🆔: <b>{character['id']}</b>",
                         parse_mode='HTML'
                     )
                 )
