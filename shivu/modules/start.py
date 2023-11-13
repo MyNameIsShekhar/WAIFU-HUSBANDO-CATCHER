8c16ea7edc879be8f6a35ea4dfbc9ce638494f83
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-13 10:28:10 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -1,11 +1,26 @@
-import random
-from motor.motor_asyncio import AsyncIOMotorClient
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, CallbackContext
 
-# MongoDB setup
-client = AsyncIOMotorClient('mongodb://localhost:27017')
-db = client['mydatabase']
+import csv
+import os
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext, CallbackQueryHandler
+from motor.motor_asyncio import AsyncIOMotorClient
+from telegram.ext import MessageHandler, filters
+
+from telegram.ext import CommandHandler
+from shivu import application 
+from shivu import db
+import random
+
+
+
+photo_url_list = ["https://graph.org/file/38767e79402baa8b04125.jpg", 
+                  "https://telegra.ph/file/c940700435ff6d27bf49d.jpg",
+                  "https://graph.org//file/11eb3e6eb3b59b844ffa8.jpg" ]
+
+    
+    
+
 collection = db['total_pm_users']
 
 photo_url_list = ["https://graph.org/file/38767e79402baa8b04125.jpg", 
@@ -35,36 +50,21 @@ async def start(update: Update, context: CallbackContext) -> None:
         photo_url = random.choice(photo_url_list)
         await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption="I am alive")
 
-     else
-       
-        photo_url = random.choice(photo_url_list)
+    else:
+        # Reply with start message
         caption = f"""
         ***Hey there! {update.effective_user.first_name} 🌻***
               
 ***i Am Collect 'Em All Bot.. Add Me in You're Group And I will send Random Characters in group after every 100 messages and who guessed that character's name Correct.. I will add That Character in That user's Collection.. Tap on help Button To See All Commands***
                """
-       
         keyboard = [
             [InlineKeyboardButton("Add Me", url=f'http://t.me/Collect_emAll_Bot?startgroup=new')],
-        
             [InlineKeyboardButton("Help", callback_data='help'),
              InlineKeyboardButton("Support", url=f'https://t.me/collect_em_all')],
             [InlineKeyboardButton("Updates", url=f'https://t.me/CollectEmAllUpdates')],
-           
-    ]
-       reply_markupp = InlineKeyboardMarkup(keyboard)
-       await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption=caption, reply_markup=reply_markupp, parse_mode='markdown')
-
-    
-    
-       
-       
-    
-
-    
-      
-    
-    
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption=caption, reply_markup=reply_markup, parse_mode='markdown')
 
 async def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
@@ -98,17 +98,13 @@ async def button(update: Update, context: CallbackContext) -> None:
         """
         keyboard = [
             [InlineKeyboardButton("Add Me", url=f'http://t.me/Collect_emAll_Bot?startgroup=new')],
-        
             [InlineKeyboardButton("Help", callback_data='help'),
              InlineKeyboardButton("Support", url=f'https://t.me/collect_em_all')],
             [InlineKeyboardButton("Updates", url=f'https://t.me/CollectEmAllUpdates')],
-           
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         
         await context.bot.edit_message_caption(chat_id=update.effective_chat.id, message_id=query.message.message_id, caption=caption, reply_markup=reply_markup, parse_mode='markdown')
-
-
 
 application.add_handler(CallbackQueryHandler(button, pattern='^help$|^back$'))
 start_handler = CommandHandler('start', start)
