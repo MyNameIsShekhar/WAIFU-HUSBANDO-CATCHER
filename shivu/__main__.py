a0001c0a4c11527bf028d9b00e902e9ad94d401d
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-21 12:38:20 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -75,7 +75,7 @@ async def message_counter(update: Update, context: CallbackContext) -> None:
         # Get message frequency for this chat from the database
         chat_frequency = await user_totals_collection.find_one({'chat_id': chat_id})
         if chat_frequency:
-            message_frequency = chat_frequency.get('message_frequency', 70)
+            message_frequency = chat_frequency.get('message_frequency', 10)
         else:
             message_frequency = 10
 
