3f4bf1b876772689bdd14475831ba46ca06c33f1
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-21 08:29:28 +0530

    Create character_appearance.py

---
+++ shivu/modules/character_appearance.py
@@ -0,0 +1,40 @@
+import importlib
+from telegram import InputMediaPhoto
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultPhoto, InputTextMessageContent, InputMediaPhoto
+from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+from itertools import groupby
+from telegram import Update
+from motor.motor_asyncio import AsyncIOMotorClient 
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, filters
+from telegram.ext import InlineQueryHandler,CallbackQueryHandler, ChosenInlineResultHandler
+from pymongo import MongoClient, ReturnDocument
+import urllib.request
+import random
+from datetime import datetime, timedelta
+from threading import Lock
+import time
+import re
+import math
+import html
+from collections import Counter
+from shivu.modules import ALL_MODULES
+from shivu import application 
+from shivu import db
+
+collection = db['anime_characters_lol']
+
+
+user_totals_collection = db['user_totals_lmaoooo']
+user_collection = db["user_collection_lmaoooo"]
+group_user_totals_collection = db['group_user_totalsssssss']
+top_global_groups_collection = db['top_global_groups']
+
+locks = {}
+message_counters = {}
+spam_counters = {}
+last_characters = {}
+sent_characters = {}
+first_correct_guesses = {}
+message_counts = {}
+ 
