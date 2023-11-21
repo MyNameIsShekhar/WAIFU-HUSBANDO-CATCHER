2f88ac02982d12cc9207a0698f28367b169eb6af
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-21 18:38:53 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -25,5 +25,10 @@ application = Application.builder().token(TOKEN).build()
 
 client = AsyncIOMotorClient('mongodb+srv://shuyaashivu:9fcc60263a946ef550d11406667404fa@cluster0.ikub9lo.mongodb.net/?retryWrites=true&w=majority')
 db = client['Character_catcher']
+collection = db['anime_characters_lol']
+user_totals_collection = db['user_totals_lmaoooo']
+user_collection = db["user_collection_lmaoooo"]
+group_user_totals_collection = db['group_user_totalsssssss']
+top_global_groups_collection = db['top_global_groups']
 
 sudo_users = ['6404226395', '6185531116', '5298587903', '5798995982', '5150644651','5813403535', '6393627898', '5952787198', '6614280216','6248411931','5216262234','1608353423']
