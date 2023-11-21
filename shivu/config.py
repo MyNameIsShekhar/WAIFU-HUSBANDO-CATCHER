6a3d3f226bba19c4515fc189b259067f3f2b755e
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-21 23:14:07 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,8 +5,8 @@ class Config(object):
     MONGO_DB_URI = "" #if u don't know how to get.. then search on YouTube
     OWNER_ID = 6765826972
     SUPPORT_CHAT = "" # add ur Support Group username Without @
-    photo_url = []
-    sudo_users = []
+    PHOTO_URL = []
+    SUDO_USERS = []
 
 
 class Production(Config):
