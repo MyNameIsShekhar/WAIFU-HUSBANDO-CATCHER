b385b44d33da15b6391023bd1536eae9a55ca7d2
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-21 23:10:54 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,10 +3,9 @@ class Config(object):
 
     TOKEN = ""  #Bot Token.. @BotFather 
     MONGO_DB_URI = "" #if u don't know how to get.. then search on YouTube
-    sudo_users = []
     OWNER_ID = 6765826972
     SUPPORT_CHAT = "" # add ur Support Group username Without @
-
+    photo_url = 
 class Production(Config):
     LOGGER = True
 
