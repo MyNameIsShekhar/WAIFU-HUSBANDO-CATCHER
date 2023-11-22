5a04b7019459f7a6f61ab97daa7f13ab60fa8e7d
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-22 15:53:16 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -5,7 +5,7 @@ from telegram.ext import CallbackContext, CallbackQueryHandler
 from telegram.ext import MessageHandler, filters
 from telegram.ext import CommandHandler
 from shivu import application 
-from shivu import db, GROUP_ID
+from shivu import db, GROUP_ID, OWNER_ID 
 from shivu import PHOTO_URL
 import random
 collection = db['total_pm_usersss']
