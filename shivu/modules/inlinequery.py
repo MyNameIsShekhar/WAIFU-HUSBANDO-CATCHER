970e6d28274d744097c0ec984337a19d6da6ba29
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-22 15:32:03 +0530

    Create inlinequery.py

---
+++ shivu/modules/inlinequery.py
@@ -0,0 +1,55 @@
+from telegram.ext import InlineQueryHandler,CallbackQueryHandler, ChosenInlineResultHandler
+from shivu import application 
+
+async def inlinequery(update: Update, context: CallbackContext) -> None:
+    from collections import Counter
+
+    query = update.inline_query.query
+    offset = int(update.inline_query.offset) if update.inline_query.offset else 0
+
+    if query.isdigit():
+        user = await user_collection.find_one({'id': int(query)})
+
+        if user:
+            # Get a list of all character IDs for the user
+            character_ids = [character['id'] for character in user['characters']]
+
+            # Count the occurrences of each character ID
+            character_counts = Counter(character_ids)
+
+            characters = list({v['id']:v for v in user['characters']}.values())[offset:offset+50]
+            if len(characters) > 50:
+                characters = characters[:50]
+                next_offset = str(offset + 50)
+            else:
+                next_offset = str(offset + len(characters))
+
+            results = []
+            for character in characters:
+                anime_characters_guessed = sum(c['anime'] == character['anime'] for c in user['characters'])
+                total_anime_characters = await collection.count_documents({'anime': character['anime']})
+
+                rarity = character.get('rarity', "Don't have rarity.. ")
+
+                count = character_counts[character['id']]
+
+                results.append(
+                    InlineQueryResultPhoto(
+                        thumbnail_url=character['img_url'],
+                        id=f"{character['id']}_{time.time()}",
+                        photo_url=character['img_url'],
+                        caption=f"<b><a href='tg://user?id={user['id']}'>{user.get('first_name', user['id'])}</a>'s Character</b>\n\n🌸: <b>{character['name']} (x{count})</b>\n🏖️: <b>{character['anime']} ({anime_characters_guessed}/{total_anime_characters})</b>\n<b>{rarity}</b>\n\n🆔: <b>{character['id']}</b> ",
+                        parse_mode='HTML'
+                    )
+                )
+
+            await update.inline_query.answer(results, next_offset=next_offset, cache_time=5)
+        else:
+            await update.inline_query.answer([InlineQueryResultArticle(
+                id='notfound', 
+                title="User not found", 
+                input_message_content=InputTextMessageContent("User not found")
+            )], cache_time=5)
+
+
+application.add_handler(InlineQueryHandler(inlinequery, block=False))
