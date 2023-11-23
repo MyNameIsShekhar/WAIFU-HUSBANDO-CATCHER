8d0e3ad7146b470c877fbbe5d0f787d2a381c6f3
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-11-23 10:18:31 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -20,7 +20,7 @@ import math
 import html
 from collections import Counter 
 from shivu import db, collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection
-from shivu import application, shivuu 
+from shivu import application, shivuu, LOGGER 
 from shivu.modules import ALL_MODULES
 
 
