cb15ca7b11b3ff1f47add5f503c3ea5bb5659e0b
epic-designer <87082611+epic-designer@users.noreply.github.com>
2023-11-25 00:14:01 +0530

    added give

---
+++ shivu/modules/dev_cmd.py
@@ -0,0 +1,93 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from motor.motor_asyncio import AsyncIOMotorClient 
+from pyrogram import Client, filters
+from pyrogram.types import InlineQueryResultPhoto, InputTextMessageContent
+from collections import Counter
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram import enums
+from shivu import db, collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection
+from shivu import shivuu
+
+
+
+sudo_users = ['6141343858']
+
+pending_gifts = {}
+
+@shivuu.on_message(filters.command("give"))
+async def gift(client, message):
+    sender_id = message.from_user.id
+
+    if not message.reply_to_message:
+        await message.reply_text("You need to reply to a user's message to gift a character!")
+        return
+
+    receiver_id = message.reply_to_message.from_user.id
+    receiver_username = message.reply_to_message.from_user.username
+    receiver_first_name = message.reply_to_message.from_user.first_name
+
+    if sender_id == receiver_id:
+        await message.reply_text("You can't gift a character to yourself!")
+        return
+
+    if len(message.command) != 2:
+        await message.reply_text("You need to provide a character ID!")
+        return
+
+    character_id = message.command[1]
+
+    sender = await user_collection.find_one({'id': sender_id})
+
+    character = next((character for character in sender['characters'] if character['id'] == character_id), None)
+
+
+    pending_gifts[(sender_id, receiver_id)] = {
+        'character': character,
+        'receiver_username': receiver_username,
+        'receiver_first_name': receiver_first_name
+    }
+
+    
+    keyboard = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("Confirm Give", callback_data="confirm_give")],
+            [InlineKeyboardButton("Cancel Give", callback_data="cancel_cancel")]
+        ]
+    )
+
+    await message.reply_text(f"do You Really Wanns To Gift {message.reply_to_message.from_user.mention}?", reply_markup=keyboard)
+
+@shivuu.on_callback_query(filters.create(lambda _, __, query: query.data in ["confirm_give", "cancel_cancel"]))
+async def on_callback_query(client, callback_query):
+    sender_id = callback_query.from_user.id
+
+    
+    for (_sender_id, receiver_id), gift in pending_gifts.items():
+        if _sender_id == sender_id:
+            break
+    else:
+        await callback_query.answer("This is not for you!", show_alert=True)
+        return
+
+    if callback_query.data == "confirm_give":
+        
+        sender = await user_collection.find_one({'id': sender_id})
+        receiver = await user_collection.find_one({'id': receiver_id})
+
+
+        
+        if receiver:
+            await user_collection.update_one({'id': receiver_id}, {'$push': {'characters': gift['character']}})
+        else:
+            
+            await user_collection.insert_one({
+                'id': receiver_id,
+                'username': gift['receiver_username'],
+                'first_name': gift['receiver_first_name'],
+                'characters': [gift['character']],
+            })
+
+        await callback_query.message.edit_text(f"You have successfully gifted your character to [{gift['receiver_first_name']}](tg://user?id={receiver_id})!")
+
+
