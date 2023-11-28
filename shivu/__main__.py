79b2dfc944b300d52bc7f95a619618b731813963
kira <87417105+SIAmKira@users.noreply.github.com>
2023-11-28 18:56:10 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -20,7 +20,7 @@ import math
 import html
 from collections import Counter 
 from shivu import db, collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection
-from shivu import application, shivuu, LOGGER 
+from shivu import application, LOGGER 
 from shivu.modules import ALL_MODULES
 
 
@@ -259,6 +259,5 @@ def main() -> None:
     application.run_polling(drop_pending_updates=True)
     
 if __name__ == "__main__":
-    shivuu.start()
     main()
 
