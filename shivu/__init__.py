170c171f5391ca4309650c02c65562915b8fc4c7
kira <87417105+SIAmKira@users.noreply.github.com>
2023-11-28 18:24:05 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -15,17 +15,18 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-OWNER_ID = os.getenv("OWNER_ID")
-sudo_users = os.getenv("SUDO_USERS").split()
-GROUP_ID = os.getenv("GROUP_ID")
-TOKEN = os.getenv("TOKEN")
-mongo_url = os.getenv("MONGO_URL")
-PHOTO_URL = os.getenv("PHOTO_URL").split()
-SUPPORT_CHAT = os.getenv("SUPPORT_CHAT")
-CHARA_CHANNEL_ID = os.getenv("CHARA_CHANNEL_ID")
-API_HASH = os.getenv("API_HASH")
-API_ID = os.getenv("API_ID")
-UPDATE_CHAT = os.getenv("UPDATE_CHAT")
+OWNER_ID = ["5715764478", "6765826972"]
+sudo_users = ["5715764478", "6765826972"]
+GROUP_ID = -1001931513350
+TOKEN = "6785717206:AAEzOZVgWP0D9H-TNo4b8HXfoe97ShUJ1B8"
+mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+PHOTO_URL = "https://graph.org/file/427df9b44c37f6d6a5eaa.jpg"
+SUPPORT_CHAT = "botsupportx"
+CHARA_CHANNEL_ID = "-1002064277119"
+API_HASH = "750432c8e1b221f91fd2c93a92710093"
+API_ID = 28122413
+UPDATE_CHAT = "botsupportx"
+BOT_USERNAME = "CatchEmWaifuBot"
 
 application = Application.builder().token(TOKEN).build()
 shivuu = Client(
