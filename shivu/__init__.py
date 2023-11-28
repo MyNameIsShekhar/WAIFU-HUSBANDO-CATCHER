ac398944e03b41817939095df5af827149125a58
MyNameIsShekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-11-28 21:21:47 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -17,9 +17,9 @@ LOGGER = logging.getLogger(__name__)
 OWNER_ID = ["5715764478", "6765826972"]
 sudo_users = ["5715764478", "6765826972"]
 GROUP_ID = -1001931513350
-TOKEN = "6970532785:AAHY_WKHYpRyYB7Aq8vaLm0lNZOWPeI5smo"
-mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-PHOTO_URL = "https://telegra.ph/file/5b48b29b86d711b5b1475.jpg", "https://telegra.ph/file/b22ecc7a43e77e62ce7dd.jpg"
+TOKEN = "6840530499:AAFEKebd8wQnVwLoTGPP6YDNZGLC7W16Lkc"
+mongo_url = "mongodb+srv://shuyaashivu:9fcc60263a946ef550d11406667404fa@cluster0.ikub9lo.mongodb.net/?retryWrites=true&w=majority"
+PHOTO_URL = "https://telegra.ph/file/5b48b29b86d711b5b1475.jpg", ""
 SUPPORT_CHAT = "botsupportx"
 CHARA_CHANNEL_ID = "-1002064277119"
 API_HASH = "750432c8e1b221f91fd2c93a92710093"
