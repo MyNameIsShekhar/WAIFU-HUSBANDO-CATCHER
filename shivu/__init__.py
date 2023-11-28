9f373e099eda810e21e5c7da075468a54ea0b0f2
MyNameIsShekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-11-28 21:24:13 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -19,13 +19,10 @@ sudo_users = ["5715764478", "6765826972"]
 GROUP_ID = -1001931513350
 TOKEN = "6840530499:AAFEKebd8wQnVwLoTGPP6YDNZGLC7W16Lkc"
 mongo_url = "mongodb+srv://shuyaashivu:9fcc60263a946ef550d11406667404fa@cluster0.ikub9lo.mongodb.net/?retryWrites=true&w=majority"
-PHOTO_URL = "https://telegra.ph/file/5b48b29b86d711b5b1475.jpg", ""
-SUPPORT_CHAT = "botsupportx"
-CHARA_CHANNEL_ID = "-1002064277119"
-API_HASH = "750432c8e1b221f91fd2c93a92710093"
-API_ID = 28122413
-UPDATE_CHAT = "botsupportx"
-BOT_USERNAME = "CatchEmWaifuBot"
+PHOTO_URL = "https://telegra.ph/file/f700d0bfe17a7c3ebe096.jpg", "https://telegra.ph/file/143eb04f0527c711dd839.jpg"
+SUPPORT_CHAT = "Collect_em_support"
+UPDATE_CHAT = "Collect_em_support"
+BOT_USERNAME = "Collect_Em_AllBot"
 
 application = Application.builder().token(TOKEN).build()
 
