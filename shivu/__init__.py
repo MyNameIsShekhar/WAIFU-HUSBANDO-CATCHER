c9880449ada5a4725f4994dd48212a3229ebc4c9
MyNameIsShekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-01 17:30:45 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -24,6 +24,7 @@ PHOTO_URL = "", ""
 SUPPORT_CHAT = "Collect_em_support"
 UPDATE_CHAT = "Collect_em_support"
 BOT_USERNAME = "Collect_Em_AllBot"
+CHARA_CHANNEL_ID = -100
 
 application = Application.builder().token(TOKEN).build()
 shivuu = Client("patanii", api_id, api_hash, bot_token=TOKEN)
