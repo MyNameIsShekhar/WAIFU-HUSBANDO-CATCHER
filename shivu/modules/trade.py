94837ded5e7450bde40eccb8cf7f27396689251c
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-03 12:34:17 +0530

    Update trade.py

--- shivu/modules/trade.py
+++ shivu/modules/trade.py
@@ -164,7 +164,7 @@ async def gift(client, message):
         ]
     )
 
-    await message.reply_text(f"do You Really Wanns To Gift {message.reply_to_message.from_user.mention}?", reply_markup=keyboard)
+    await message.reply_text(f"do You Really Wanns To Gift {message.reply_to_message.from_user.mention} ?", reply_markup=keyboard)
 
 @shivuu.on_callback_query(filters.create(lambda _, __, query: query.data in ["confirm_gift", "cancel_gift"]))
 async def on_callback_query(client, callback_query):
