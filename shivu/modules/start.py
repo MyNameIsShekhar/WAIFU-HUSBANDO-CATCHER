f96ab259598426e1c887ec5e399b225a5f218f15
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-03 11:05:44 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -96,7 +96,7 @@ async def button(update: Update, context: CallbackContext) -> None:
 
         
         keyboard = [
-            [InlineKeyboardButton("ADD ME", url=f'http://t.me/CatchEmWaifuBot?startgroup=new')],
+            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("HELP", callback_data='help')],
