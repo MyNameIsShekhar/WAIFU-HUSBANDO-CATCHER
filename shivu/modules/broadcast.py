6653c842ab0750280d14b41fc767ba5de0a802b5
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-04 18:11:11 +0530

    Create broadcast.py

---
+++ shivu/modules/broadcast.py
@@ -0,0 +1,42 @@
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackContext
+
+import application from shivu, top_global_groups_collection, pm_users, OWNER_ID 
+
+async def broadcast(update: Update, context: CallbackContext) -> None:
+    
+    if update.effective_user.id != OWNER_ID:
+        await update.message.reply_text("You are not authorized to use this command.")
+        return
+
+    
+    message_to_broadcast = update.message.reply_to_message
+
+    if message_to_broadcast is None:
+        await update.message.reply_text("Please reply to a message to broadcast.")
+        return
+
+  
+    all_chats = await top_global_groups_collection.distinct("group_id")
+    all_users = await pm_users.distinct("_id")
+
+    shuyaa = list(set(all_chats + all_users))
+
+    failed_sends = 0
+
+    for chat_id in shuyaa:
+        try:
+            
+            await context.bot.copy_message(chat_id=chat_id,
+                                           from_chat_id=message_to_broadcast.chat_id,
+                                           message_id=message_to_broadcast.message_id)
+        except Exception as e:
+            print(f"Failed to send message to {chat_id}: {e}")
+            failed_sends += 1
+
+    await update.message.reply_text(f"Broadcast complete. Failed to send to {failed_sends} chats/users.")
+
+
+application.add_handler(CommandHandler("broadcast", broadcast, block=False)
