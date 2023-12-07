c59b2ed18b48cd774b59377ffb487de416096acb
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-07 13:40:07 +0530

    Update inlinequery.py

--- shivu/modules/inlinequery.py
+++ shivu/modules/inlinequery.py
@@ -1,23 +1,20 @@
 import re
 import time
 from html import escape
+from pyrogram import Client, filters, types
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from telegram import Update, InlineQueryResultPhoto
-from telegram.ext import InlineQueryHandler, CallbackContext
+from shivu import user_collection, collection, shivuu
 
-from shivu import user_collection, collection, application
 
-async def inlinequery(update: Update, context: CallbackContext) -> None:
-    query = update.inline_query.query
-    offset = int(update.inline_query.offset) if update.inline_query.offset else 0
+async def inlinequery(client: Client, query: types.InlineQuery):
+    offset = int(query.offset) if query.offset else 0
 
-    
-    if query.startswith('collection.'):
-        user_id, *search_terms = query.split(' ')[0].split('.')[1], ' '.join(query.split(' ')[1:])
+    if query.text.startswith('collection.'):
+        user_id, *search_terms = query.text.split(' ')[0].split('.')[1], ' '.join(query.text.split(' ')[1:])
         if user_id.isdigit():
-            user = await user_collection.find_one({'id': int(user_id)})
+            user = await db.user_collection.find_one({'id': int(user_id)})
             if user:
-                
                 all_characters = list({v['id']:v for v in user['characters']}.values())
                 if search_terms:
                     regex = re.compile(' '.join(search_terms), re.IGNORECASE)
@@ -27,11 +24,11 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
         else:
             all_characters = []
     else:
-        if query:
-            regex = re.compile(query, re.IGNORECASE)
-            all_characters = list(await collection.find({"$or": [{"name": regex}, {"anime": regex}]}).to_list(length=None))
+        if query.text:
+            regex = re.compile(query.text, re.IGNORECASE)
+            all_characters = list(await db.collection.find({"$or": [{"name": regex}, {"anime": regex}]}).to_list(length=None))
         else:
-            all_characters = list(await collection.find({}).to_list(length=None))
+            all_characters = list(await db.collection.find({}).to_list(length=None))
 
     characters = all_characters[offset:offset+50]
     if len(characters) > 50:
@@ -42,18 +39,18 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
 
     results = []
     for character in characters:
-        global_count = await user_collection.count_documents({'characters.id': character['id']})
-        anime_characters = await collection.count_documents({'anime': character['anime']})
+        global_count = await db.user_collection.count_documents({'characters.id': character['id']})
+        anime_characters = await db.collection.count_documents({'anime': character['anime']})
 
-        if query.startswith('collection.'):
+        if query.text.startswith('collection.'):
             user_character_count = sum(c['id'] == character['id'] for c in user['characters'])
             user_anime_characters = sum(c['anime'] == character['anime'] for c in user['characters'])
             caption = f"<b> Look At <a href='tg://user?id={user['id']}'>{(escape(user.get('first_name', user['id'])))}</a>'s Character</b>\n\n🌸: <b>{character['name']} (x{user_character_count})</b>\n🏖️: <b>{character['anime']} ({user_anime_characters}/{anime_characters})</b>\n<b>{character['rarity']}</b>\n\n<b>🆔️:</b> {character['id']}"
         else:
             caption = f"<b>Look At This Character !!</b>\n\n🌸:<b> {character['name']}</b>\n🏖️: <b>{character['anime']}</b>\n<b>{character['rarity']}</b>\n🆔️: <b>{character['id']}</b>\n\n<b>Globally Guessed {global_count} Times...</b>"
         results.append(
-            InlineQueryResultPhoto(
-                thumbnail_url=character['img_url'],
+            types.InlineQueryResultPhoto(
+                thumb_url=character['img_url'],
                 id=f"{character['id']}_{time.time()}",
                 photo_url=character['img_url'],
                 caption=caption,
@@ -61,6 +58,7 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
             )
         )
 
-    await update.inline_query.answer(results, next_offset=next_offset, cache_time=5)
+    await client.answer_inline_query(query.id, results, next_offset=next_offset, cache_time=5, is_gallery=True)
 
-application.add_handler(InlineQueryHandler(inlinequery, block=False))
+shivuu.add_handler(filters.inline_query, inlinequery)
+
