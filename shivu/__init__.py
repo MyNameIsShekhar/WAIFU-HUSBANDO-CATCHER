7f15d4ddb38d6ad715d7a9daa45bd6a92718147f
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-09 09:56:05 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -5,7 +5,7 @@ from pyrogram import Client
 from telegram.ext import Application
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from shivu import config 
+from shivu import config *
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -18,18 +18,18 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-OWNER_ID = config.OWNER_ID
-sudo_users = config.sudo_users
-GROUP_ID = config.GROUP_ID
-TOKEN = config.TOKEN
-mongo_url = config.mongo_url
-PHOTO_URL = config.PHOTO_URL
-SUPPORT_CHAT = config.SUPPORT_CHAT
-UPDATE_CHAT = config.UPDATE_CHAT
-BOT_USERNAME = config.BOT_USERNAME
-CHARA_CHANNEL_ID = config.CHARA_CHANNEL_ID
-api_id = config.api_id
-api_hash = config.api_hash
+OWNER_ID = OWNER_ID
+sudo_users = sudo_users
+GROUP_ID = GROUP_ID
+TOKEN = TOKEN
+mongo_url = mongo_url
+PHOTO_URL = PHOTO_URL
+SUPPORT_CHAT = SUPPORT_CHAT
+UPDATE_CHAT = UPDATE_CHAT
+BOT_USERNAME = BOT_USERNAME
+CHARA_CHANNEL_ID = CHARA_CHANNEL_ID
+api_id = api_id
+api_hash = api_hash
 
 
 application = Application.builder().token(TOKEN).build()
