287e537f649689e742da3256544908f6ad32efb2
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-23 22:35:10 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = ""
-    sudo_users = "", ""
-    GROUP_ID = -100
-    TOKEN = ""
-    mongo_url = ""
+    OWNER_ID = "6765826972"
+    sudo_users = "6845325416", "6765826972"
+    GROUP_ID = -1002133191051
+    TOKEN = "6785717206:AAEzOZVgWP0D9H-TNo4b8HXfoe97ShUJ1B8"
+    mongo_url = "mongodb+srv://shuyaaaaa12:NvpoBuRp7MVPcAYA@cluster0.q2yycqx.mongodb.net/"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
     BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = ""
-    api_id = ""
-    api_hash = ""
+    CHARA_CHANNEL_ID = "-1002133191051"
+    api_id = "6433468"
+    api_hash = "7895dfd061f656367ccab30032"
 
     
 class Production(Config):
