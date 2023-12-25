67a56f0d9af32e0f44abb339fe15c4526997415b
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-26 02:08:11 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "6765826972"
     sudo_users = "6845325416", "6765826972"
     GROUP_ID = -1002133191051
-    TOKEN = "6653243642:AAEaI93MNElP8WXsQ59AZWZgvnZ1WpnVTyY"
+    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
     mongo_url = "mongodb+srv://shuyaaaaa12:NvpoBuRp7MVPcAYA@cluster0.q2yycqx.mongodb.net/"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
