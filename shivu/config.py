e96ded785674142bf20365e336edcf21c019a9b7
Sinughosh <152395454+Sinughosh@users.noreply.github.com>
2023-12-25 12:46:49 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,14 +6,14 @@ class Config(object):
     sudo_users = "6845325416", "6765826972"
     GROUP_ID = -1002133191051
     TOKEN = "6653243642:AAEaI93MNElP8WXsQ59AZWZgvnZ1WpnVTyY"
-    mongo_url = "mongodb+srv://shuyaaaaa12:NvpoBuRp7MVPcAYA@cluster0.q2yycqx.mongodb.net/"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    mongo_url = "mongodb+srv://ixbotmongodb:RadheRadhe@cluster0.ga1q2fn.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://telegra.ph/file/b5b4b464581fc56c2a690.jpg", "https://telegra.ph/file/60f16b14f097252e4b412.jpg"]
+    SUPPORT_CHAT = "Grab_Your_Characters_6"
+    UPDATE_CHAT = "Grab_Your_Characters_6"
+    BOT_USERNAME = "Grab_Your_Husbando_IxBot"
+    CHARA_CHANNEL_ID = "-1001873432374"
+    api_id = 20967639
+    api_hash = "f0cc55917e860f93d2c9aaa4a7caa69c"
 
     
 class Production(Config):
