d05c76596a410b3242057c592598b3e7ca0b9672
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2023-12-26 02:31:06 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "6845325416", "6765826972"
     GROUP_ID = -1002133191051
     TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://shuyaashivu:9fcc60263a946ef550d11406667404fa@cluster0.ikub9lo.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv:/sjejjejejej/?jrjrjrj=true&w=nsnsnsns"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
